261eb82e6526e7d644d7d5b8ce2afca533be0571
Wil Clouser <wclouser@mozilla.com>
2017-07-12 14:34:18 -0700

    update build script

--- bin/build-addon.sh
+++ bin/build-addon.sh
@@ -3,15 +3,12 @@ set -ex
 npm install
 
 if [[ -z $TESTPILOT_AMO_USER || -z $TESTPILOT_AMO_SECRET ]]; then
-  rm -f ./*.zip
-  NODE_ENV=development npm run package
-  mv voicefill*.zip voicefill-dev.zip
-  NODE_ENV=production npm run package
+  echo "No vars set. skipping build..."
+  exit 1
 else
-  NODE_ENV=production npm run build
   rm -f ./web-ext-artifacts/*.xpi
   ./node_modules/.bin/web-ext sign \
-    --source-dir dist \
+    --source-dir extension \
     --api-key $TESTPILOT_AMO_USER \
     --api-secret $TESTPILOT_AMO_SECRET
   mv ./web-ext-artifacts/*.xpi ./signed-addon.xpi
