fb9671aca0cbc998791e111c59577e8befa2fcff
Andre Natal <anatal@gmail.com>
2017-07-25 13:42:18 -0700

    Adding more metrics.

--- extension/metrics.js
+++ extension/metrics.js
@@ -4,37 +4,73 @@
 
 class Metrics {
 
-     constructor() {
+    constructor() {
          this.num_attempts = 0;
      }
 
-     session() {
-         console.log("session");
+    start_session(triggered_by_uielement) {
+        this.num_attempts = 0;
+        this.accepted_confidence = -1;
+        this.accepted_idx_suggestion = -1;
+        this.accepted_outcome = "";
+        this.triggered_by_uielement = triggered_by_uielement;
+    }
+
+    end_session() {
          // move to library
          this.sendMessage("session", {
              sc: "end",
-             cm1: "", // the number of attempts made in a session.
-             cm2: "", // the confidence level of the accepted suggestion, if one was accepted; otherwise omitted. Integer between 1 and 100, inclusive.
-             cm3: "", // the index of an accepted suggestion, if one was accepted; otherwise omitted.
-             cm4: "", //  the elapsed time in ms spent recording an attempt.
-             cm5: "", // the elapsed time in ms waiting for a response from the speech-to-text engine.
-             cd1: "", //  the outcome of a session or attempt. One of accepted, rejected, and aborted.
-             cd2: "", // the location from which a session is initiated. One of google, duckduckgo, Yahoo, generic.
-             cd3: "", // the UI element from which the session was initiated. One of button, context menu, keyboard.
-             cd4: "" // whether the accepted submission was modified before being submitted. One of true, false.
+             cm1: this.num_attempts, // the number of attempts made in a session.
+             cm2: this.accepted_confidence, // the confidence level of the accepted suggestion, if one was accepted; otherwise omitted. Integer between 1 and 100, inclusive.
+             cm3: this.accepted_idx_suggestion, // the index of an accepted suggestion, if one was accepted; otherwise omitted.
+             cm4: this.dt_recording_time, //  the elapsed time in ms spent recording an attempt.
+             cm5: this.dt_response_time, //  the elapsed time in ms waiting for a response from the speech-to-text engine.
+             cd1: this.accepted_outcome, //  the outcome of a session or attempt. One of accepted, rejected, and aborted.
+             cd2: window.location.hostname, //  the location from which a session is initiated. One of google, duckduckgo, Yahoo, generic.
+             cd3: this.triggered_by_uielement, //  the UI element from which the session was initiated. One of button, context menu, keyboard.
+             cd4: this.was_result_modified  //   whether the accepted submission was modified before being submitted. One of true, false.
          });
      }
 
-    attempt(confidence_level, idx_suggestion, recording_time, response_time, user_outcome, was_options_displayed) {
-         this.num_attempts += 1;
-         console.log("attempt:", this.num_attempts);
+    start_recording() {
+        this.dt_recording_time =  Date.now();
+    }
+
+    stop_recording() {
+        this.dt_recording_time = Date.now() - this.dt_recording_time;
+    }
+
+    start_attempt() {
+        this.num_attempts += 1;
+        this.options_were_displayed = 0;
+    }
+
+    start_stt() {
+        this.dt_response_time = Date.now();
+    }
+
+    end_stt() {
+        this.dt_response_time = Date.now() - this.dt_response_time;
+    }
+
+    set_options_displayed() {
+        this.options_were_displayed = 1;
+    }
+
+    end_attempt(confidence_level, attempt_outcome, idx_suggestion, result_modified) {
+         console.log("[metrics] metrics.js attempt:", this.num_attempts);
+         this.accepted_confidence = confidence_level * 100;
+         this.accepted_idx_suggestion = idx_suggestion;
+         this.accepted_outcome = attempt_outcome;
+         this.was_result_modified = result_modified;
+
          this.sendMessage("attempt", {
              cm2: confidence_level * 100, // the confidence level of the accepted suggestion, if one was accepted; otherwise omitted. Integer between 1 and 100, inclusive.
              cm3: idx_suggestion, // the index of an accepted suggestion, if one was accepted; otherwise omitted.
-             cm4: recording_time, //  the elapsed time in ms spent recording an attempt.
-             cm5: response_time, // the elapsed time in ms waiting for a response from the speech-to-text engine.
-             cd1: user_outcome, //  the outcome of a session or attempt. One of accepted, rejected, and aborted.
-             cd5: was_options_displayed //  whether the user viewed additional suggestions.
+             cm4: this.dt_recording_time, //  the elapsed time in ms spent recording an attempt.
+             cm5: this.dt_response_time, // the elapsed time in ms waiting for a response from the speech-to-text engine.
+             cd1: attempt_outcome, //  the outcome of a session or attempt. One of accepted, rejected, and aborted.
+             cd5: this.options_were_displayed //  whether the user viewed additional suggestions.
          });
     }
 
