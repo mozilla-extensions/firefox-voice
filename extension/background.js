813867933c21b8e29940747c3bc628e1750e23a8
Andre Natal <anatal@gmail.com>
2017-11-20 23:12:20 -0800

    1.3.6

--- extension/background.js
+++ extension/background.js
@@ -9,7 +9,7 @@ const analytics = new TestPilotGA({
   ds: 'addon',
   an: 'Voice Fill',
   aid: 'voicefill@mozilla.com',
-  av: '1.3.1'
+  av: '1.3.6'
 });
 
 browser.runtime.onMessage.addListener(event => {
