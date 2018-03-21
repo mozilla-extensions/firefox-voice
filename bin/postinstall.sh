38a9a71a7489edcb9be9a37528ad561f0e1e3f47
Greg Guthe <gguthe@mozilla.com>
2018-03-21 18:37:32 -0400

    remove DOMPurify

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -3,4 +3,3 @@ set -ex
 
 cp node_modules/bodymovin/build/player/bodymovin.js extension/bodymovin.js
 cp node_modules/testpilot-ga/dist/index.js extension/testpilot-ga.js
-cp node_modules/dompurify/dist/purify.min.js extension/purify.min.js
