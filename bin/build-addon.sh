0f389caf9b23051b338fa9cdaa2a408769b66cff
Michael Stegeman <michael@stegeman.me>
2018-10-18 07:29:43 -0600

    Update dependencies.
    Fixes #114

--- bin/build-addon.sh
+++ bin/build-addon.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 set -ex
-scriptdir=$(dirname $(readlink -f "$0"))
 
 npm install
-
-# Generate changelog for extension
-"${scriptdir}/../node_modules/.bin/md2html" \
-  "${scriptdir}/../CHANGELOG.md" > "${scriptdir}/../extension/CHANGELOG.html"
 
 if [[ -z $TESTPILOT_AMO_USER || -z $TESTPILOT_AMO_SECRET ]]; then
   echo "No vars set. skipping build..."
--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+
 set -ex
 
-cp node_modules/bodymovin/build/player/bodymovin.js extension/bodymovin.js
-cp node_modules/testpilot-ga/dist/index.js extension/testpilot-ga.js
+# Generate changelog for extension
+scriptdir=$(dirname $(readlink -f "$0"))
+"${scriptdir}/../node_modules/.bin/md2html" \
+  "${scriptdir}/../CHANGELOG.md" > "${scriptdir}/../extension/CHANGELOG.html"
+
+# Copy in scripts from node modules
+cp \
+  "${scriptdir}/../node_modules/bodymovin/build/player/bodymovin.min.js" \
+  "${scriptdir}/../extension/bodymovin.js"
+cp \
+  "${scriptdir}/../node_modules/testpilot-ga/dist/index.js" \
+  "${scriptdir}/../extension/testpilot-ga.js"
+cp \
+  "${scriptdir}/../node_modules/webrtcvad_js/webrtc_vad.js" \
+  "${scriptdir}/../extension/webrtc_vad.js"
