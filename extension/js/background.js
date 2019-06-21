67bd964e28d67a8be811560c307e58665800958a
Julia Cambre <jcambre@andrew.cmu.edu>
2019-06-20 17:21:05 -0700

    Add text shortcut to trigger the extension through the awesome bar

--- extension/js/background.js
+++ extension/js/background.js
@@ -9,6 +9,21 @@ browser.browserAction.onClicked.addListener(async (triggeringTab) => {
   triggeringTabId = triggeringTab.id;
   console.debug(`the tab that the user was on when triggering this action has ID ${triggeringTabId}`);
 
+  triggerExtension();
+});
+
+browser.omnibox.setDefaultSuggestion({
+  description: `Control Firefox through a text command (e.g. Play Hamilton on YouTube)`
+});
+
+browser.omnibox.onInputEntered.addListener(async (text, disposition) => {
+  // Figure out if the new text thing is empty
+  triggerExtension();
+  // browser.tabs.update({url});
+
+});
+
+const triggerExtension = async () => {
   const url = "https://www.google.com";
   let tab = await browser.tabs.create({url});
   const intervalConnection = setInterval(() => {
@@ -23,4 +38,4 @@ browser.browserAction.onClicked.addListener(async (triggeringTab) => {
         // console.error(`Not connected yet. Retrying ${error}`);
       });
   }, 100);
-});
+}
