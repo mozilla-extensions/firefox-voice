3bf685c238373969ffe339936aedbdce43bcbdcf
Julia Cambre <jcambre@andrew.cmu.edu>
2019-08-12 21:06:25 -0700

    Processing state with a bit of fancier CSS

--- extension/popup/vad.js
+++ extension/popup/vad.js
@@ -156,6 +156,9 @@ this.vad = (function() {
       log.debug("goCloud, why:", why);
       this.stopGum();
       // FIXME: maybe we need to signal the UI here?
+      if (why === "GoCloud finishedvoice") {
+        ui.setState("processing");
+      }
     }
   };
 
