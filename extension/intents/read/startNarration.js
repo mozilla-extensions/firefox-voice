64b185c4d703740d1dd91c1280192f3e981e2be9
Ian Bicking <ian@ianbicking.org>
2019-08-23 16:18:35 -0500

    Fix #110, open narrate controls in reader mode

--- extension/intents/read/startNarration.js
+++ extension/intents/read/startNarration.js
@@ -1,4 +1,8 @@
 function startNarration() {
+  const dropdown = document.querySelector(".narrate-dropdown");
+  if (dropdown) {
+    dropdown.classList.add("open");
+  }
   const element = document.querySelector(".narrate-start-stop");
   if (element) {
     element.click();
