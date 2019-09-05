83b1bef2b1b574eb48fa0ae60147284d9d77eb30
Ian Bicking <ian@ianbicking.org>
2019-09-05 16:39:45 -0500

    Update CircleCI config to generate XPI update file
    Also makes version dynamic based on timestamp(ish)

---
+++ bin/generate-update-json.js
@@ -0,0 +1,18 @@
+// See https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/Updates
+
+const version = process.env.VERSION;
+const update_link = process.env.UPDATE_BASE + "/firefox-voice.xpi";
+if (!version || !update_link) {
+  throw new Error("Both $VERSION and $UPDATE_BASE must be set");
+}
+const json = {
+  addons: {
+    "firefox-voice@mozilla.org": [
+      {
+        version,
+        update_link,
+      },
+    ],
+  },
+};
+process.stdout.write(JSON.stringify(json, null, "  "));
