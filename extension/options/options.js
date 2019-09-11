bb4e7cceba834ee98d2c4be4f80e1cc11d99ecf7
Ian Bicking <ian@ianbicking.org>
2019-09-11 11:53:18 -0500

    Fix #214, include version and build date in settings

--- extension/options/options.js
+++ extension/options/options.js
@@ -98,10 +98,25 @@ async function init() {
   const inDevelopment = await browser.runtime.sendMessage({
     type: "inDevelopment",
   });
-  console.log("result", inDevelopment);
   if (inDevelopment) {
     document.body.classList.add("inDevelopment");
   }
+  const manifest = browser.runtime.getManifest();
+  document.getElementById("version").textContent = manifest.version;
+  document.getElementById("buildTime").textContent =
+    manifest.settings.buildTime;
+  let dirty = false;
+  let { gitCommit } = manifest.settings;
+  if (gitCommit.endsWith("-dirty")) {
+    dirty = true;
+    gitCommit = gitCommit.split("-")[0];
+  }
+  const anchor = document.getElementById("gitCommit");
+  anchor.textContent = gitCommit;
+  anchor.href = `https://github.com/mozilla/firefox-voice/commit/${encodeURIComponent(
+    gitCommit
+  )}`;
+  document.getElementById("dirty").style.display = dirty ? "" : "none";
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
