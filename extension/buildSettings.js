6f408e6a6dc502d5b30b86ba194b723817c8d6b9
Ian Bicking <ian@ianbicking.org>
2019-09-19 14:42:38 -0500

    Fix #282, replace add-on icon in about:addons

--- extension/buildSettings.js
+++
@@ -1,9 +0,0 @@
-this.buildSettings = {
-  logLevel: "debug",
-  inDevelopment: null,
-  sstServer: "https://speaktome-2.services.mozilla.com",
-  gitCommit: "a747d2f-dirty",
-  buildTime: "2019-09-19T19:31:03.154Z",
-  backgroundTabRecorder: true,
-  channel: "unknown",
-};
