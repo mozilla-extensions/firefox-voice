b846b11dc0ba5f6d07bac279e35fc937038cab8f
Ian Bicking <ian@ianbicking.org>
2019-10-11 17:06:02 -0500

    Add a new 'fancy' search technique as a test
    This does a google search in a background (pinned) tab, maybe displays the card in the popup, and maybe opens the first search result.
    
    Allows going through the search results item by item with 'fancy next'

---
+++ extension/intents/search/queryScript.js
@@ -0,0 +1,42 @@
+/* globals communicate */
+
+this.queryScript = (function() {
+  const CARD_SELECTOR = ".vk_c.card-section";
+
+  communicate.register("searchResultInfo", message => {
+    const hasSidebarCard = !!document.querySelector(".kp-header");
+    const hasCard = !!document.querySelector(CARD_SELECTOR);
+    const searchHeaders = document.querySelectorAll("a > h3");
+    const searchResults = [];
+    for (const searchHeader of searchHeaders) {
+      searchResults.push({
+        url: searchHeader.parentNode.href,
+        title: searchHeader.textContent,
+      });
+    }
+    return {
+      hasSidebarCard,
+      hasCard,
+      searchResults,
+    };
+  });
+
+  communicate.register("cardImage", message => {
+    const card = document.querySelector(CARD_SELECTOR);
+    const rect = card.getBoundingClientRect();
+    const canvas = document.createElementNS(
+      "http://www.w3.org/1999/xhtml",
+      "canvas"
+    );
+    canvas.width = rect.width; // * window.devicePixelRatio
+    canvas.height = rect.height; // * window.devicePixelRatio
+    const ctx = canvas.getContext("2d");
+    // ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+    ctx.drawWindow(window, rect.x, rect.y, rect.width, rect.height, "#fff");
+    return {
+      width: rect.width,
+      height: rect.height,
+      src: canvas.toDataURL(),
+    };
+  });
+})();
