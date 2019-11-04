mozilla-extensions/firefox-voice

commit 808f57cd395cd0801e235bac1083a0aafc2fe8d9

React and most popup functionality (#507)
* Fix #491, convert the popup to use React
* Remove popup.js and add to gitignore. It is auto-generated by script "npm run build:jsx"
* Fixes Issue #502 Remove card rendering

extension/popup/popupController.jsx
@@ -1,0 +1,354 @@
+/* globals React, ReactDOM, util, voice, vad, settings, log, voiceShim, buildSettings, lottie */
+
+const { useState, useEffect } = React;
+const popupContainer = document.getElementById("popup-container");
+let isInitialized = false;
+let textInputDetected = false;
+
+function PopupController() {
+  const [currentView, setCurrentView] = useState("listening");
+  const [suggestions, setSuggestions] = useState([]);
+  const [transcript, setTranscript] = useState(null);
+  const [displayText, setDisplayText] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [displayAutoplay, setDisplayAutoplay] = useState(false);
+  const [searchResult, setSearchResult] = useState(null);
+  const [recorderVolume, setRecorderVolume] = useState(null);
+
+  let recorder;
+  let recorderIntervalId;
+  let executedIntent = false;
+  let stream = null;
+  const { backgroundTabRecorder } = buildSettings;
+  let isWaitingForPermission = false;
+  const FAST_PERMISSION_CLOSE = 500;
+  const PERMISSION_REQUEST_TIME = 2000;
+  // Default amount of time (in milliseconds) before the action is automatically dismissed after we perform certain actions (e.g. successfully switching to a different open tab). This value should give users enough time to read the content on the popup before it closes.
+  const DEFAULT_TIMEOUT = 2500;
+  // Timeout for the popup when there's text displaying:
+  const TEXT_TIMEOUT = 7000;
+  let overrideTimeout;
+
+  useEffect(() => {
+    if (!isInitialized) {
+      isInitialized = true;
+      init();
+    }
+  });
+
+  const init = async () => {
+    backgroundTabRecorder
+      ? await voiceShim.openRecordingTab()
+      : await setupStream();
+
+    recorder = backgroundTabRecorder
+      ? new voiceShim.Recorder()
+      : new voice.Recorder(stream);
+
+    const userSettings = await settings.getSettings();
+    if (userSettings.chime) {
+      playListeningChime();
+    }
+
+    addListeners();
+    updateExamples();
+    startRecorder();
+  };
+
+  const showSettings = async () => {
+    await browser.tabs.create({
+      url: browser.runtime.getURL("options/options.html"),
+    });
+    window.close();
+  };
+
+  const playListeningChime = () => {
+    const audio = new Audio("https://jcambre.github.io/vf/mic_open_chime.ogg");
+    audio.play();
+  };
+
+  const onClickLexicon = async () => {
+    await browser.tabs.create({
+      url: browser.runtime.getURL(event.target.href),
+    });
+    window.close();
+  };
+
+  const handleMessage = message => {
+    switch (message.type) {
+      case "closePopup": {
+        closePopup(message.time);
+        break;
+      }
+      case "displayFailure": {
+        //setCurrentView("error");
+        setErrorMessage(message.message);
+        break;
+      }
+      case "displayText": {
+        setDisplayText(message.message);
+        overrideTimeout = TEXT_TIMEOUT;
+        break;
+      }
+      case "displayAutoplayFailure": {
+        displayAutoplayFailure();
+        break;
+      }
+      case "showSearchResults": {
+        showSearchResults(message);
+        return Promise.resolve(true);
+      }
+      case "refreshSearchCard": {
+        if (!message.card) {
+          throw new Error(
+            ".card property missing on refreshSearchCard message"
+          );
+        }
+        showSearchCard(message.card);
+        return Promise.resolve(true);
+      }
+      default:
+        break;
+    }
+    return undefined;
+  };
+
+  const updateExamples = async () => {
+    const suggestions = await browser.runtime.sendMessage({
+      type: "getExamples",
+      number: 3,
+    });
+
+    setSuggestions(suggestions);
+  };
+
+  const onKeyPressed = () => {
+    setPopupView("typing");
+    if (!textInputDetected) {
+      textInputDetected = true;
+      onStartTextInput();
+    }
+  };
+
+  const onStartTextInput = async () => {
+    await browser.runtime.sendMessage({ type: "microphoneStopped" });
+    log.debug("detected text from the popup");
+    recorder.cancel(); // not sure if this is working as expected?
+  };
+
+  const submitTextInput = async text => {
+    if (text) {
+      setTranscript(text);
+      setPopupView("success");
+      executedIntent = true;
+      await browser.runtime.sendMessage({ type: "microphoneStopped" });
+      browser.runtime.sendMessage({
+        type: "addTelemetry",
+        properties: { inputTyped: true },
+      });
+      browser.runtime.sendMessage({
+        type: "runIntent",
+        text,
+      });
+    }
+  };
+
+  const setPopupView = async newView => {
+    setCurrentView(newView);
+
+    if (recorder && !recorderIntervalId && newView === "listening") {
+      recorderIntervalId = setInterval(() => {
+        setVolumeForAnimation(recorder.getVolumeLevel());
+      }, 500);
+    } else {
+      clearInterval(recorderIntervalId);
+    }
+  };
+
+  const setVolumeForAnimation = newRecorderVolume => {
+    setRecorderVolume(newRecorderVolume);
+  };
+
+  const displayAutoplayFailure = () => {
+    setDisplayAutoplay(true);
+    setErrorMessage(
+      "Please enable autoplay on this site for a better experience"
+    );
+  };
+
+  const showSearchResults = newSearch => {
+    setCurrentView("searchResults");
+    setSearchResult(newSearch);
+
+    if (newSearch.card) {
+      setMinPopupSize(card.width, card.height);
+    }
+  };
+
+  const showSearchCard = newCard => {
+    if (newCard) {
+      const newSearch = { ...search };
+      newSearch.card = newCard;
+
+      setSearchResult(newSearch);
+      setMinPopupSize(newCard.width, newCard.height);
+    }
+  };
+
+  const onSearchImageClick = async searchUrl => {
+    await browser.runtime.sendMessage({
+      type: "focusSearchResults",
+      searchUrl: searchUrl,
+    });
+  };
+
+  const setIcon = state => {
+    browser.browserAction.setIcon({
+      16: `${state}-16.svg`,
+      32: `${state}-32.svg`,
+    });
+  };
+
+  const setMinPopupSize = (width, height) => {
+    popupContainer.style.minWidth = width + "px";
+    popupContainer.style.minHeight = parseInt(height) + 150 + "px";
+  };
+
+  // TODO: Console Warning: "Scripts may not close windows that were not opened by script."
+  const closePopup = ms => {
+    if (ms === null || ms === undefined) {
+      ms = overrideTimeout ? overrideTimeout : DEFAULT_TIMEOUT;
+    }
+
+    // TODO: offload mic and other resources before closing?
+    setTimeout(() => {
+      window.close();
+    }, ms);
+  };
+
+  const addListeners = () => {
+    document.addEventListener("keydown", onKeyPressed);
+
+    // Listen for messages from the background scripts
+    browser.runtime.onMessage.addListener(handleMessage);
+
+    window.addEventListener("unload", () => {
+      alert("here");
+      document.removeEventListener("keydown", onKeyPressed);
+
+      if (
+        !backgroundTabRecorder &&
+        isWaitingForPermission &&
+        Date.now() - isWaitingForPermission < FAST_PERMISSION_CLOSE
+      ) {
+        startOnboarding();
+      }
+
+      browser.runtime.sendMessage({ type: "microphoneStopped" });
+      if (!executedIntent) {
+        browser.runtime.sendMessage({ type: "cancelledIntent" });
+      }
+    });
+  };
+
+  const startRecorder = () => {
+    recorder.onBeginRecording = () => {
+      setPopupView("listening");
+      browser.runtime.sendMessage({ type: "microphoneStarted" });
+    };
+    recorder.onEnd = json => {
+      setPopupView("success");
+      clearInterval(recorderIntervalId);
+      executedIntent = true;
+      // Probably superfluous, since this is called in onProcessing:
+      browser.runtime.sendMessage({ type: "microphoneStopped" });
+      if (json === null) {
+        // It was cancelled
+        return;
+      }
+      setTranscript(json.data[0].text);
+      browser.runtime.sendMessage({
+        type: "addTelemetry",
+        properties: { transcriptionConfidence: json.data[0].confidence },
+      });
+      browser.runtime.sendMessage({
+        type: "runIntent",
+        text: json.data[0].text,
+      });
+    };
+    recorder.onError = error => {
+      setPopupView("error");
+      clearInterval(recorderIntervalId);
+      browser.runtime.sendMessage({ type: "microphoneStopped" });
+      log.error("Got recorder error:", String(error), error);
+    };
+    recorder.onProcessing = () => {
+      setPopupView("processing");
+      browser.runtime.sendMessage({ type: "microphoneStopped" });
+    };
+    recorder.onNoVoice = () => {
+      browser.runtime.sendMessage({ type: "microphoneStopped" });
+      log.debug("Closing popup because of no voice input");
+      window.close();
+    };
+    recorder.startRecording();
+  };
+
+  const setupStream = async () => {
+    try {
+      isWaitingForPermission = Date.now();
+      await startMicrophone();
+      isWaitingForPermission = false;
+    } catch (e) {
+      isWaitingForPermission = false;
+      if (e.name === "NotAllowedError" || e.name === "TimeoutError") {
+        startOnboarding();
+        window.close();
+        return;
+      }
+      throw e;
+    }
+
+    await vad.stm_vad_ready;
+  };
+
+  const requestMicrophone = async () => {
+    stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    return stream;
+  };
+
+  const startMicrophone = async () => {
+    const sleeper = util.sleep(PERMISSION_REQUEST_TIME).then(() => {
+      const exc = new Error("Permission Timed Out");
+      exc.name = "TimeoutError";
+      throw exc;
+    });
+    await Promise.race([requestMicrophone(), sleeper]);
+  };
+
+  const startOnboarding = async () => {
+    await browser.tabs.create({
+      url: browser.extension.getURL("onboarding/onboard.html"),
+    });
+  };
+
+  return (
+    <Popup
+      currentView={currentView}
+      suggestions={suggestions}
+      transcript={transcript}
+      displayText={displayText}
+      errorMessage={errorMessage}
+      displayAutoplay={displayAutoplay}
+      searchResult={searchResult}
+      recorderVolume={recorderVolume}
+      showSettings={showSettings}
+      submitTextInput={submitTextInput}
+      onClickLexicon={onClickLexicon}
+      onSearchImageClick={onSearchImageClick}
+      setMinPopupSize={setMinPopupSize}
+    />
+  );
+}
+
+ReactDOM.render(<PopupController />, popupContainer);

extension/popup/popupView.jsx
@@ -1,0 +1,506 @@
+/* globals lottie */
+
+const { Component, PureComponent } = React;
+
+const Popup = ({
+  currentView,
+  suggestions,
+  transcript,
+  displayText,
+  errorMessage,
+  displayAutoplay,
+  searchResult,
+  recorderVolume,
+  showSettings,
+  submitTextInput,
+  onClickLexicon,
+  onSearchImageClick,
+  setMinPopupSize,
+}) => {
+  return (
+    <div id="popup" className={currentView}>
+      <PopupHeader currentView={currentView} />
+      <PopupContent
+        currentView={currentView}
+        suggestions={suggestions}
+        transcript={transcript}
+        displayText={displayText}
+        errorMessage={errorMessage}
+        displayAutoplay={displayAutoplay}
+        searchResult={searchResult}
+        recorderVolume={recorderVolume}
+        submitTextInput={submitTextInput}
+        onClickLexicon={onClickLexicon}
+        onSearchImageClick={onSearchImageClick}
+        setMinPopupSize={setMinPopupSize}
+      />
+      <PopupFooter showSettings={showSettings} />
+    </div>
+  );
+};
+
+const PopupHeader = ({ currentView }) => {
+  const getTitle = () => {
+    switch (currentView) {
+      case "processing":
+        return "One second...";
+      case "success":
+        return "Got it!";
+      case "error":
+        return "Sorry, there was an issue";
+      case "typing":
+        return "Type your request";
+      case "searchResults":
+        return "Search results";
+      case "listening":
+      default:
+        return "Listening";
+    }
+  };
+
+  const onClickGoBack = () => {
+    location.href = `${location.pathname}?${Date.now()}`;
+  };
+
+  const onClickClose = () => {
+    window.close();
+  };
+
+  const hiddenClass =
+    currentView === "error" || currentView === "searchResults" ? "" : "hidden";
+
+  return (
+    <div id="popup-header">
+      <div id="left-icon" className={hiddenClass} onClick={onClickGoBack}>
+        <svg
+          id="back-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill="context-fill"
+            d="M6.414 8l4.293-4.293a1 1 0 0 0-1.414-1.414l-5 5a1 1 0 0 0 0 1.414l5 5a1 1 0 0 0 1.414-1.414z"
+          ></path>
+        </svg>
+      </div>
+      <div id="header-title">{getTitle()}</div>
+      <div id="close-icon" onClick={onClickClose}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill="context-fill"
+            d="M9.061 8l3.47-3.47a.75.75 0 0 0-1.061-1.06L8 6.939 4.53 3.47a.75.75 0 1 0-1.06 1.06L6.939 8 3.47 11.47a.75.75 0 1 0 1.06 1.06L8 9.061l3.47 3.47a.75.75 0 0 0 1.06-1.061z"
+          ></path>
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+const PopupContent = ({
+  currentView,
+  suggestions,
+  transcript,
+  displayText,
+  errorMessage,
+  displayAutoplay,
+  searchResult,
+  recorderVolume,
+  submitTextInput,
+  onClickLexicon,
+  onSearchImageClick,
+  setMinPopupSize,
+}) => {
+  const getContent = () => {
+    switch (currentView) {
+      case "listening":
+        return (
+          <ListeningContent
+            displayText={displayText}
+            suggestions={suggestions}
+            onClickLexicon={onClickLexicon}
+          />
+        );
+      case "typing":
+        return (
+          <TypingContent
+            displayText={displayText}
+            submitTextInput={submitTextInput}
+          />
+        );
+      case "processing":
+        return (
+          <ProcessingContent
+            transcript={transcript}
+            displayText={displayText}
+          />
+        );
+      case "success":
+        return (
+          <SuccessContent transcript={transcript} displayText={displayText} />
+        );
+      case "error":
+        return (
+          <ErrorContent
+            displayText={displayText}
+            errorMessage={errorMessage}
+            displayAutoplay={displayAutoplay}
+          />
+        );
+      case "searchResults":
+        return (
+          <SearchResultsContent
+            search={searchResult}
+            displayText={displayText}
+            onSearchImageClick={onSearchImageClick}
+            setMinPopupSize={setMinPopupSize}
+          />
+        );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div id="popup-content">
+      <Zap currentView={currentView} recorderVolume={recorderVolume} />
+      {getContent()}
+    </div>
+  );
+};
+
+const PopupFooter = ({ showSettings }) => {
+  return (
+    <div id="popup-footer">
+      <div id="settings-icon" onClick={showSettings}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill="context-fill"
+            d="M15 7h-2.1a4.967 4.967 0 0 0-.732-1.753l1.49-1.49a1 1 0 0 0-1.414-1.414l-1.49 1.49A4.968 4.968 0 0 0 9 3.1V1a1 1 0 0 0-2 0v2.1a4.968 4.968 0 0 0-1.753.732l-1.49-1.49a1 1 0 0 0-1.414 1.415l1.49 1.49A4.967 4.967 0 0 0 3.1 7H1a1 1 0 0 0 0 2h2.1a4.968 4.968 0 0 0 .737 1.763c-.014.013-.032.017-.045.03l-1.45 1.45a1 1 0 1 0 1.414 1.414l1.45-1.45c.013-.013.018-.031.03-.045A4.968 4.968 0 0 0 7 12.9V15a1 1 0 0 0 2 0v-2.1a4.968 4.968 0 0 0 1.753-.732l1.49 1.49a1 1 0 0 0 1.414-1.414l-1.49-1.49A4.967 4.967 0 0 0 12.9 9H15a1 1 0 0 0 0-2zM5 8a3 3 0 1 1 3 3 3 3 0 0 1-3-3z"
+          ></path>
+        </svg>
+      </div>
+      <div id="moz-voice-privacy">
+        <strong>For Mozilla internal use only</strong>
+        {/* <a href="">How Mozilla ensures voice privacy</a> */}
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+const ListeningContent = ({ displayText, suggestions, onClickLexicon }) => {
+  return (
+    <React.Fragment>
+      <TextDisplay displayText={displayText} />
+      <VoiceInput suggestions={suggestions} onClickLexicon={onClickLexicon} />
+    </React.Fragment>
+  );
+};
+
+const TypingContent = ({ displayText, submitTextInput }) => {
+  return (
+    <React.Fragment>
+      <TextDisplay displayText={displayText} />
+      <TypingInput submitTextInput={submitTextInput} />
+    </React.Fragment>
+  );
+};
+
+const VoiceInput = ({ suggestions, onClickLexicon }) => {
+  const onMoreSuggestions = event => {
+    if (event) {
+      event.preventDefault();
+      onClickLexicon(event);
+    }
+  };
+  return (
+    <div id="voice-input">
+      {suggestions ? (
+        <div id="suggestions">
+          <p id="prompt">You can say things like:</p>
+          <div id="suggestions-list">
+            {suggestions.map(suggestion => (
+              <p className="suggestion" key={suggestion}>
+                {suggestion}
+              </p>
+            ))}
+          </div>
+          <div>
+            <a
+              target="_blank"
+              rel="noopener"
+              id="lexicon"
+              href="../views/lexicon.html"
+              onClick={onMoreSuggestions}
+            >
+              More things you can say
+            </a>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+// TODO: test if componentDidMount can be removed? Convert to functional component?
+class TypingInput extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.textInputRef = React.createRef();
+    this.value = null;
+  }
+
+  componentDidMount() {
+    this.focusText();
+  }
+
+  focusText = () => {
+    if (this.textInputRef.current) {
+      setTimeout(() => {
+        this.textInputRef.current.focus();
+      }, 0);
+    }
+  };
+
+  onInputKeyPress = event => {
+    if (event.key === "Enter") {
+      this.submitTextInput();
+    }
+  };
+
+  onInputTextChange = () => {
+    this.value = event.target.value;
+  };
+
+  submitTextInput = async () => {
+    const text = this.value;
+
+    if (text) {
+      this.props.submitTextInput(text);
+    }
+  };
+
+  render() {
+    return (
+      <div id="text-input">
+        <input
+          type="text"
+          id="text-input-field"
+          autoFocus="1"
+          onKeyPress={this.onInputKeyPress}
+          onChange={this.onInputTextChange}
+        />
+        <div id="send-btn-wrapper">
+          <button id="send-text-input" onClick={this.submitTextInput}>
+            GO
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+const ProcessingContent = ({ transcript, displayText }) => {
+  return (
+    <React.Fragment>
+      <Transcript transcript={transcript} />
+      <TextDisplay displayText={displayText} />
+    </React.Fragment>
+  );
+};
+
+const SuccessContent = ({ transcript, displayText }) => {
+  return (
+    <React.Fragment>
+      <Transcript transcript={transcript} />
+      <TextDisplay displayText={displayText} />
+    </React.Fragment>
+  );
+};
+
+const ErrorContent = ({ displayText, errorMessage, displayAutoplay }) => {
+  return (
+    <div>
+      <TextDisplay displayText={displayText} />
+      {errorMessage ? <div id="error-message">{errorMessage}</div> : null}
+      {displayAutoplay ? (
+        <div id="error-autoplay">
+          <img
+            src="images/autoplay-instruction.png"
+            alt="To enable autoplay, open the site preferences and select Allow Audio and Video"
+          />
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+const SearchResultsContent = ({
+  search,
+  displayText,
+  onSearchImageClick,
+  setMinPopupSize,
+}) => {
+  if (!search) return null;
+
+  const { card, searchResults, index, searchUrl } = search;
+  const next = searchResults[index + 1];
+  const cardStyles = card ? { height: card.height, width: card.width } : {};
+  const imgAlt = next ? next.title : "";
+
+  if (card) setMinPopupSize(card.width, card.height);
+
+  const onSearchCardClick = () => {
+    onSearchImageClick(searchUrl);
+  };
+
+  return (
+    <React.Fragment>
+      <TextDisplay displayText={displayText} />
+      <div id="search-results">
+        {/* FIXME: img alt is using "next" title, but it should use the card title, which is not available in the data  */}
+        {card ? (
+          <img
+            id="search-image"
+            alt={imgAlt}
+            onClick={onSearchCardClick}
+            style={cardStyles}
+            src={card.src}
+          />
+        ) : null}
+        {next ? (
+          <div id="search-show-next">
+            Say <strong>next result</strong> to view: <br />
+            <strong id="search-show-next-title">{next.title}</strong>
+            <span id="search-show-next-domain">
+              {new URL(next.url).hostname}
+            </span>
+          </div>
+        ) : null}
+      </div>
+    </React.Fragment>
+  );
+};
+
+const Transcript = ({ transcript }) => {
+  return transcript ? <div id="transcript">{transcript}</div> : null;
+};
+
+const TextDisplay = ({ displayText }) => {
+  return displayText ? <div id="text-display">{displayText}</div> : null;
+};
+
+class Zap extends Component {
+  constructor(props) {
+    super(props);
+
+    this.animation = null;
+
+    this.animationSegmentTimes = {
+      reveal: [0, 14],
+      base: [14, 30],
+      low: [30, 46],
+      medium: [46, 62],
+      high: [62, 78],
+      processing: [78, 134],
+      error: [134, 153],
+      success: [184, 203],
+    };
+
+    this.animationConfig = {
+      listening: {
+        segments: [
+          this.animationSegmentTimes.reveal,
+          this.animationSegmentTimes.base,
+        ],
+        loop: true,
+        interrupt: true,
+      },
+      processing: {
+        segments: [this.animationSegmentTimes.processing],
+        loop: false,
+        interrupt: false,
+      },
+      success: {
+        segments: [this.animationSegmentTimes.success],
+        loop: false,
+        interrupt: false,
+      },
+      error: {
+        segments: [this.animationSegmentTimes.error],
+        loop: false,
+        interrupt: false,
+      },
+    };
+  }
+
+  componentDidMount() {
+    this.loadAnimation();
+  }
+
+  componentDidUpdate() {
+    const config = this.animationConfig[this.props.currentView];
+
+    if (config) {
+      this.playAnimation(config.segments, config.interrupt, config.loop);
+
+      if (this.props.currentView === "listening" && this.props.recorderVolume) {
+        this.setAnimationForVolume(this.props.recorderVolume);
+      }
+    }
+  }
+
+  loadAnimation = async () => {
+    this.animation = await lottie.loadAnimation({
+      container: document.getElementById("zap"), // the dom element that will contain the animation
+      loop: false,
+      renderer: "svg",
+      autoplay: false,
+      path: "animations/Firefox_Voice_Full.json", // the path to the animation json
+    });
+  };
+
+  playAnimation = (segments, interrupt, loop) => {
+    if (this.animation) {
+      this.animation.loop = loop;
+      this.animation.playSegments(segments, interrupt);
+    }
+  };
+
+  setAnimationForVolume = avgVolume => {
+    console.log("setAnimationForVolume", avgVolume);
+    //this.animation.onLoopComplete = function() {
+    if (avgVolume < 0.1) {
+      this.playAnimation(this.animationSegmentTimes.base, true, true);
+    } else if (avgVolume < 0.15) {
+      this.playAnimation(this.animationSegmentTimes.low, true, true);
+    } else if (avgVolume < 0.2) {
+      this.playAnimation(this.animationSegmentTimes.medium, true, true);
+    } else {
+      this.playAnimation(this.animationSegmentTimes.high, true, true);
+    }
+    //};
+  };
+
+  render() {
+    return this.props.currentView !== "typing" &&
+      this.props.currentView !== "searchResults" ? (
+      <div id="zap-wrapper">
+        <div id="zap"></div>
+      </div>
+    ) : null;
+  }
+}

.eslintignore
@@ -1,1 +1,3 @@
 extension/js/vendor/
+extension/popup/popupController.js
+extension/popup/popupView.js

.eslintrc.yml
@@ -4,12 +4,16 @@ env:
   node: true
   webextensions: true
 
+ecmaFeatures:
+  jsx: true
+
 extends:
   - eslint:recommended
   - plugin:mozilla/recommended
 
 plugins:
   - mozilla
+  - react
 
 globals:
   Metrics: true

.gitignore
@@ -7,6 +7,8 @@ extension/css/vendor/
 extension/views/CHANGELOG.html
 extension/views/lexicon.html
 extension/buildSettings.js
+extension/popup/popupController.js
+extension/popup/popupView.js
 node_modules
 npm-debug.log
 package-lock.json

commit c3dcf395eb5449c4c9c45a920077484bf8c7c1a0

Multiple fixes to card images
- Fixes #530, normal cards were not displayed. The filtering was based on the card's .bottom value, which is a relative coordinate, but should have been based on the .y value
- Fixes #529, next result works, probably due to a an exception that kept the popup result from being saved
- Fixes #528, fix animated images, fixes a regression from #507 where updating the card image was broken
- Fixes #514, showing sidebar car. This was mostly working, but a small logic bug only showed the sidebar card if there was also a main card

extension/intents/search/queryScript.js
@@ -18,7 +18,7 @@ this.queryScript = (function() {
     let selected = container.querySelectorAll(CARD_SELECTOR);
     if (maxBottom) {
       selected = Array.from(selected).filter(
-        e => e.getBoundingClientRect().bottom <= maxBottom
+        e => e.getBoundingClientRect().y <= maxBottom
       );
     }
     if (selected.length) {

extension/intents/search/search.js
@@ -145,7 +145,7 @@ this.intents.search = (function() {
       popupSearchInfo = null;
       await performSearch(context.slots.query);
       const searchInfo = await callScript({ type: "searchResultInfo" });
-      if (searchInfo.hasCard) {
+      if (searchInfo.hasCard || searchInfo.hasSidebarCard) {
         const card = await callScript({ type: "cardImage" });
         context.keepPopup();
         searchInfo.index = -1;

extension/popup/popupController.jsx
@@ -13,6 +13,7 @@ function PopupController() {
   const [errorMessage, setErrorMessage] = useState(null);
   const [displayAutoplay, setDisplayAutoplay] = useState(false);
   const [searchResult, setSearchResult] = useState(null);
+  const [cardImage, setCardImage] = useState(null);
   const [recorderVolume, setRecorderVolume] = useState(null);
 
   let recorder;
@@ -176,21 +177,25 @@ function PopupController() {
     );
   };
 
-  const showSearchResults = newSearch => {
+  const showSearchResults = message => {
     setCurrentView("searchResults");
+    const newSearch = {};
+    for (const prop in message) {
+      if (prop !== "type" && prop !== "card") {
+        newSearch[prop] = message[prop];
+      }
+    }
     setSearchResult(newSearch);
 
-    if (newSearch.card) {
-      setMinPopupSize(card.width, card.height);
+    if (message.card) {
+      setCardImage(message.card);
+      setMinPopupSize(message.card.width, message.card.height);
     }
   };
 
   const showSearchCard = newCard => {
+    setCardImage(newCard);
     if (newCard) {
-      const newSearch = { ...search };
-      newSearch.card = newCard;
-
-      setSearchResult(newSearch);
       setMinPopupSize(newCard.width, newCard.height);
     }
   };
@@ -341,6 +346,7 @@ function PopupController() {
       errorMessage={errorMessage}
       displayAutoplay={displayAutoplay}
       searchResult={searchResult}
+      cardImage={cardImage}
       recorderVolume={recorderVolume}
       showSettings={showSettings}
       submitTextInput={submitTextInput}

extension/popup/popupView.jsx
@@ -10,6 +10,7 @@ const Popup = ({
   errorMessage,
   displayAutoplay,
   searchResult,
+  cardImage,
   recorderVolume,
   showSettings,
   submitTextInput,
@@ -28,6 +29,7 @@ const Popup = ({
         errorMessage={errorMessage}
         displayAutoplay={displayAutoplay}
         searchResult={searchResult}
+        cardImage={cardImage}
         recorderVolume={recorderVolume}
         submitTextInput={submitTextInput}
         onClickLexicon={onClickLexicon}
@@ -111,6 +113,7 @@ const PopupContent = ({
   errorMessage,
   displayAutoplay,
   searchResult,
+  cardImage,
   recorderVolume,
   submitTextInput,
   onClickLexicon,
@@ -157,6 +160,7 @@ const PopupContent = ({
         return (
           <SearchResultsContent
             search={searchResult}
+            cardImage={cardImage}
             displayText={displayText}
             onSearchImageClick={onSearchImageClick}
             setMinPopupSize={setMinPopupSize}
@@ -350,13 +354,15 @@ const ErrorContent = ({ displayText, errorMessage, displayAutoplay }) => {
 
 const SearchResultsContent = ({
   search,
+  cardImage,
   displayText,
   onSearchImageClick,
   setMinPopupSize,
 }) => {
   if (!search) return null;
 
-  const { card, searchResults, index, searchUrl } = search;
+  const { searchResults, index, searchUrl } = search;
+  const card = cardImage;
   const next = searchResults[index + 1];
   const cardStyles = card ? { height: card.height, width: card.width } : {};
   const imgAlt = next ? next.title : "";