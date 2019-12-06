c429a1716c3c91847cadc6348225872b32c7f02e
Jennifer Harmon <29240718+jenniferharmon@users.noreply.github.com>
2019-12-06 07:38:05 -0800

    Adds hover state to popup icon buttons

--- extension/popup/popupView.jsx
+++ extension/popup/popupView.jsx
@@ -105,14 +105,13 @@ this.popupView = (function() {
 
     return (
       <div id="popup-header">
-        <div id="left-icon" className={hiddenClass} onClick={onClickGoBack}>
+        <button id="left-icon" className={hiddenClass} onClick={onClickGoBack}>
           <svg
             id="back-icon"
             xmlns="http://www.w3.org/2000/svg"
             width="16"
             height="16"
             viewBox="0 0 16 16"
-            role="button"
             aria-label="Back"
           >
             <path
@@ -120,14 +119,9 @@ this.popupView = (function() {
               d="M6.414 8l4.293-4.293a1 1 0 0 0-1.414-1.414l-5 5a1 1 0 0 0 0 1.414l5 5a1 1 0 0 0 1.414-1.414z"
             ></path>
           </svg>
-        </div>
+        </button>
         <div id="header-title">{getTitle()}</div>
-        <div
-          id="close-icon"
-          role="button"
-          aria-label="Close"
-          onClick={onClickClose}
-        >
+        <button id="close-icon" aria-label="Close" onClick={onClickClose}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="16"
@@ -139,7 +133,7 @@ this.popupView = (function() {
               d="M9.061 8l3.47-3.47a.75.75 0 0 0-1.061-1.06L8 6.939 4.53 3.47a.75.75 0 1 0-1.06 1.06L6.939 8 3.47 11.47a.75.75 0 1 0 1.06 1.06L8 9.061l3.47 3.47a.75.75 0 0 0 1.06-1.061z"
             ></path>
           </svg>
-        </div>
+        </button>
       </div>
     );
   };
