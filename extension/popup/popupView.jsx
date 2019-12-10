6602861a023dc930d0c23f604e6f0b9a17c3586e
maitrella <maitrella@gmail.com>
2019-12-10 14:40:18 +0100

    Delete changes in popup

--- extension/popup/popupView.jsx
+++ extension/popup/popupView.jsx
@@ -548,7 +548,6 @@ this.popupView = (function() {
     const { searchResults, index, searchUrl } = search;
     const card = cardImage;
     const next = searchResults[index + 1];
-    const previous = searchResults[index - 1];
     const cardStyles = card ? { height: card.height, width: card.width } : {};
     const imgAlt =
       card && card.alt
@@ -583,15 +582,6 @@ this.popupView = (function() {
               <strong id="search-show-next-title">{next.title}</strong>
               <span id="search-show-next-domain">
                 {new URL(next.url).hostname}
-              </span>
-            </div>
-          ) : null}
-          {previous ? (
-            <div id="search-show-previous">
-              Say <strong>previous result</strong> to view: <br />
-              <strong id="search-show-previous-title">{previous.title}</strong>
-              <span id="search-show-previous-domain">
-                {new URL(previous.url).hostname}
               </span>
             </div>
           ) : null}
