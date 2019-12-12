a27b0c1c01640e4a2a03ec7ec1c12610c6495ad5
Ian Bicking <ian@ianbicking.org>
2019-12-12 14:33:19 -0600

    Add src=update to update links

--- bin/generate-update-json.js
+++ bin/generate-update-json.js
@@ -1,7 +1,7 @@
 // See https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/Updates
 
 const version = process.env.VERSION;
-const update_link = process.env.UPDATE_BASE + "/firefox-voice.xpi";
+const update_link = process.env.UPDATE_BASE + "/firefox-voice.xpi?src=update";
 if (!version || !update_link) {
   throw new Error("Both $VERSION and $UPDATE_BASE must be set");
 }
