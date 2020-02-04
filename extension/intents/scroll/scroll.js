2d4a949466e5897ed6deedfb2b2bb02862a3ad3a
maitrella <maitrella@gmail.com>
2020-02-04 12:00:47 +0100

    Format changes after "npm run format"

--- extension/intents/scroll/scroll.js
+++ extension/intents/scroll/scroll.js
@@ -1,63 +1,63 @@
 /* globals content */
 
 this.intentRunner.registerIntent({
-    name: "scroll.up",
-    description: "Scroll the current page up",
-    examples: ["Scroll up"],
-    match: `
+  name: "scroll.up",
+  description: "Scroll the current page up",
+  examples: ["Scroll up"],
+  match: `
     scroll (this |) (tab | page | article |) (up | upward)
     page up
   `,
-    async run(context) {
-        const activeTab = await context.activeTab();
-        await content.lazyInject(activeTab.id, "intents/scroll/scrollHelper.js");
-        await browser.tabs.sendMessage(activeTab.id, {type: "scrollUp"});
-    },
+  async run(context) {
+    const activeTab = await context.activeTab();
+    await content.lazyInject(activeTab.id, "intents/scroll/scrollHelper.js");
+    await browser.tabs.sendMessage(activeTab.id, { type: "scrollUp" });
+  },
 });
 
 this.intentRunner.registerIntent({
-    name: "scroll.down",
-    description: "Scroll the current page down",
-    examples: ["Scroll down"],
-    match: `
+  name: "scroll.down",
+  description: "Scroll the current page down",
+  examples: ["Scroll down"],
+  match: `
     scroll (this |) (tab | page | article |) (down | downward)
     page down
   `,
-    async run(context) {
-        const activeTab = await context.activeTab();
-        await content.lazyInject(activeTab.id, "intents/scroll/scrollHelper.js");
-        await browser.tabs.sendMessage(activeTab.id, {type: "scrollDown"});
-    },
+  async run(context) {
+    const activeTab = await context.activeTab();
+    await content.lazyInject(activeTab.id, "intents/scroll/scrollHelper.js");
+    await browser.tabs.sendMessage(activeTab.id, { type: "scrollDown" });
+  },
 });
 
 this.intentRunner.registerIntent({
-    name: "scroll.top",
-    description: "Scroll the current page all the way to the top",
-    examples: ["Scroll all the way up"],
-    match: `
+  name: "scroll.top",
+  description: "Scroll the current page all the way to the top",
+  examples: ["Scroll all the way up"],
+  match: `
         scroll (this |) (tab | page | article |) all the way (up | upward) (to the top |)
         scroll (this |) (tab | page | article |) (all the way |) to (the |) (very |) top
         page all the way (up | to the top | to top)
       `,
-    async run(context) {
-        const activeTab = await context.activeTab();
-        await content.lazyInject(activeTab.id, "intents/scroll/scrollHelper.js");
-        await browser.tabs.sendMessage(activeTab.id, {type: "scrollToTop"});
-    },
- });
+  async run(context) {
+    const activeTab = await context.activeTab();
+    await content.lazyInject(activeTab.id, "intents/scroll/scrollHelper.js");
+    await browser.tabs.sendMessage(activeTab.id, { type: "scrollToTop" });
+  },
+});
 
- this.intentRunner.registerIntent({
-    name: "scroll.bottom",
-    description: "Scroll the current page all the way to the bottom",
-    examples: ["Scroll all the way down"],
-    match: `
+this.intentRunner.registerIntent({
+  name: "scroll.bottom",
+  description: "Scroll the current page all the way to the bottom",
+  examples: ["Scroll all the way down"],
+  match: `
         scroll (this |) (tab | page | article |) all the way (down | downward |) (to the bottom |)
         scroll (this |) (tab | page | article |) (all the way |) to (the |) (very |) bottom
         page all the way (down | to the bottom | to bottom)
       `,
-    async run(context) {
-        const activeTab = await context.activeTab();
-        await content.lazyInject(activeTab.id, "intents/scroll/scrollHelper.js");
-        await browser.tabs.sendMessage(activeTab.id, {type: "scrollToBottom"});
-    },
- });
+  async run(context) {
+    const activeTab = await context.activeTab();
+    await content.lazyInject(activeTab.id, "intents/scroll/scrollHelper.js");
+    await browser.tabs.sendMessage(activeTab.id, { type: "scrollToBottom" });
+  },
+});
--- extension/intents/scroll/scrollHelper.js
+++ extension/intents/scroll/scrollHelper.js
@@ -1,36 +1,36 @@
 /* globals communicate */
 
-const scrollAmount = .9;
+const scrollAmount = 0.9;
 
 function scrollVertically(dy, smooth) {
-    window.scrollBy({
-        left: 0,
-        top: dy,
-        behavior: smooth ? "smooth" : "auto"
-    });
-    return true;
+  window.scrollBy({
+    left: 0,
+    top: dy,
+    behavior: smooth ? "smooth" : "auto",
+  });
+  return true;
 }
 
 function scrollUp() {
-    return scrollVertically(-scrollAmount * window.innerHeight, true);
+  return scrollVertically(-scrollAmount * window.innerHeight, true);
 }
 
 communicate.register("scrollUp", scrollUp);
 
 function scrollDown() {
-    return scrollVertically(scrollAmount * window.innerHeight, true);
+  return scrollVertically(scrollAmount * window.innerHeight, true);
 }
 
 communicate.register("scrollDown", scrollDown);
 
 function scrollToTop() {
-    return scrollVertically(-window.scrollY, false);
+  return scrollVertically(-window.scrollY, false);
 }
 
 communicate.register("scrollToTop", scrollToTop);
 
 function scrollToBottom() {
-    return scrollVertically(document.body.scrollHeight - window.scrollY, false);
+  return scrollVertically(document.body.scrollHeight - window.scrollY, false);
 }
 
 communicate.register("scrollToBottom", scrollToBottom);
