a2f2e13ae9dcf3c552ced0605012abd7a499680e
Ellen Spertus <ellen.spertus@gmail.com>
2020-02-24 11:24:19 -0800

    Complete unit tests for map intent

--- app/src/main/java/mozilla/voice/assistant/intents/maps/Maps.kt
+++ app/src/main/java/mozilla/voice/assistant/intents/maps/Maps.kt
@@ -13,30 +13,37 @@ class Maps {
         internal const val LOCATION_KEY = "location"
         @VisibleForTesting
         internal const val THING_KEY = "thing"
-        private const val MODE_KEY = "mode"
-        private const val BIKE_MODE = 'b'
-        private const val WALK_MODE = 'w'
+        @VisibleForTesting
+        internal const val MODE_KEY = "mode"
+        @VisibleForTesting
+        internal const val BIKE_MODE = "b"
+        @VisibleForTesting
+        internal const val WALK_MODE = "w"
 
         fun register() {
             IntentRunner.registerIntent(
                 mozilla.voice.assistant.Intent(
                     "Maps - navigate",
                     "Navigate to a specific destination",
-                    listOf("Show me how to get to Carnegie Hall", "Navigate to 345 Spear Street"),
                     listOf(
-                        "(show|tell) me (how to get|how to drive|directions|the way) to [$LOCATION_KEY]",
+                        "Show me how to get to Carnegie Hall",
+                        "Navigate to 345 Spear Street",
+                        "What's the best way to walk to the Bay Bridge?"
+                    ),
+                    listOf(
                         "(give me|i'd like) (car|driving|) directions to [$LOCATION_KEY]",
                         "navigate (by car|) to [$LOCATION_KEY]",
                         "(show|tell) (me|) how to (bike|bicycle|cycle) to [$LOCATION_KEY] [$MODE_KEY=$BIKE_MODE]",
                         "(show|tell) (me|) how to get to [$LOCATION_KEY] by (bike|biking|bicycle|bicycling|cycling) [$MODE_KEY=$BIKE_MODE]",
                         "(show|tell) (me|) how to walk to [$LOCATION_KEY] [$MODE_KEY=$WALK_MODE]",
-                        "(show|tell) (me|) how to get to [$LOCATION_KEY] (by walking|on foot) $MODE_KEY=$WALK_MODE]",
-                        "what's the (best|fastest|quickest) way to get to [$LOCATION_KEY] (by car|)",
-                        "what's the (best|fastest|quickest) way to drive to [$LOCATION_KEY] (by car|)",
-                        "what's the (best|fastest|quickest) way to get to [$LOCATION_KEY] (on foot|by walking) [$MODE_KEY=$WALK_MODE]",
-                        "what's the (best|fastest|quickest) way to walk to [$LOCATION_KEY] [$MODE_KEY=$WALK_MODE]",
-                        "what's the (best|fastest|quickest) way to get to [$LOCATION_KEY] (by|on) (bike|biking|bicycle|bicycling|cycling) [$MODE_KEY=$BIKE_MODE]",
-                        "what's the (best|fastest|quickest) way to (bike|bicycle|cycle) to [$LOCATION_KEY] [$MODE_KEY=$BIKE_MODE]"
+                        "(show|tell) (me|) how to get to [$LOCATION_KEY] (on foot|by foot|by walking) [$MODE_KEY=$WALK_MODE]",
+                        "(show|tell) me (how to get|how to drive|directions|the way) to [$LOCATION_KEY]",
+                        "(what's|show me|tell me) the (best|fastest|quickest) way to get to [$LOCATION_KEY] (by car|)",
+                        "(what's|show me|tell me) the (best|fastest|quickest) way to drive to [$LOCATION_KEY] (by car|)",
+                        "(what's|show me|tell me) the (best|fastest|quickest) way to get to [$LOCATION_KEY] (on foot|by foot|by walking) [$MODE_KEY=$WALK_MODE]",
+                        "(what's|show me|tell me) the (best|fastest|quickest) way to walk to [$LOCATION_KEY] [$MODE_KEY=$WALK_MODE]",
+                        "(what's|show me|tell me) the (best|fastest|quickest) way to get to [$LOCATION_KEY] (by|on) (bike|biking|bicycle|bicycling|cycling) [$MODE_KEY=$BIKE_MODE]",
+                        "(what's|show me|tell me) the (best|fastest|quickest) way to (bike|bicycle|cycle) to [$LOCATION_KEY] [$MODE_KEY=$BIKE_MODE]"
                     ),
                     ::createNavigateIntent
                 )
@@ -48,11 +55,11 @@ class Maps {
                     "Find a certain type of place",
                     listOf("Where can I buy pizza?", "What's a good place to get sushi?", "Where's good ramen in Oakland?"),
                     listOf(
-                        "where (can i|to) (get|find|buy|eat) [$THING_KEY] (in|near|) [$LOCATION_KEY]",
+                        "where (can i|to) (get|find|buy|eat) [$THING_KEY] (in|near) [$LOCATION_KEY]",
                         "where (can i|to) (get|find|buy|eat) [$THING_KEY]",
-                        "(where's|where is|what's|what is) (the|a) (near|nearest|close|closest|good|best|) (place for|place to get|) [$THING_KEY] (in|near|) [$LOCATION_KEY]",
+                        "(where's|where is|what's|what is) (the|a) (near|nearest|close|closest|good|best|) (place for|place to get|) [$THING_KEY] (in|near) [$LOCATION_KEY]",
                         "(where's|where is|what's|what is) (the|a) (near|nearest|close|closest|good|best|) (place for|place to get|) [$THING_KEY]",
-                        "(search for|look for|find) nearby [$THING_KEY] (in|near|) [$LOCATION_KEY]",
+                        "(search for|look for|find) [$THING_KEY] (in|near) [$LOCATION_KEY]",
                         "(search for|look for|find) nearby [$THING_KEY]"
                     ),
                     ::createSearchIntent
--- app/src/test/java/mozilla/voice/assistant/MapsTest.kt
+++ app/src/test/java/mozilla/voice/assistant/MapsTest.kt
@@ -13,11 +13,26 @@ class MapsTest {
     private fun testMapHelper(
         utterance: String,
         expectedThing: String? = null,
-        expectedLocation: String? = null
+        expectedLocation: String? = null,
+        expectedMode: String? = null
     ) {
         IntentParser.getBestMatch(utterance)?.let { mr: MatcherResult ->
-            expectedThing?.let { assertEquals(it, mr.slots[Maps.THING_KEY]) }
-            expectedLocation?.let { assertEquals(it, mr.slots[Maps.LOCATION_KEY]) }
+            expectedThing?.let { assertEquals(
+                "incorrect thing for: $utterance",
+                it,
+                mr.slots[Maps.THING_KEY]
+            ) }
+            expectedLocation?.let { assertEquals(
+                "incorrect location for: $utterance",
+                it,
+                mr.slots[Maps.LOCATION_KEY]
+            ) }
+            expectedMode?.let { assertEquals(
+                "incorrect mode for: $utterance",
+                it,
+                mr.parameters[Maps.MODE_KEY]
+            ) }
+            true // to keep from triggering below Elvis operator
         } ?: fail("Did not parse as map request: $utterance")
     }
 
@@ -25,10 +40,79 @@ class MapsTest {
     fun testLocationMatches() {
         listOf(
             Pair("tell me how to get to LOC", "sesame street"),
+            Pair("show me directions to LOC", "berkeley"),
             Pair("i'd like driving directions to LOC", "mills college"),
-            Pair("navigate to LOC", "mozilla hq")
+            Pair("give me directions to LOC", "dairy queen"),
+            Pair("navigate to LOC", "mozilla hq"),
+            Pair("navigate by car to LOC", "chinatown"),
+            Pair("what's the best way to get to LOC", "the bay bridge"),
+            Pair("what's the quickest way to drive to LOC", "seattle")
         ).map {
-            testMapHelper(it.first.replace("LOC", it.second), expectedLocation = it.second)
+            testMapHelper(
+                it.first.replace("LOC", it.second),
+                expectedLocation = it.second)
+        }
+    }
+
+    @Test
+    fun testLocationBikeMatches() {
+        val loc = "mozilla hq"
+        listOf(
+            "tell me how to bike to $loc",
+            "show how to bicycle to $loc",
+            "show how to get to $loc by bike",
+            "show how to cycle to $loc",
+            "what's the quickest way to bike to $loc"
+        ).map {
+            testMapHelper(it, expectedMode = Maps.BIKE_MODE)
+        }
+    }
+
+    @Test
+    fun testLocationWalkingMatches() {
+        val loc = "mozilla hq"
+        listOf(
+            "tell me how to walk to $loc",
+            "show how to get to $loc on foot",
+            "show how to get to $loc by foot",
+            "show me how to get to $loc on foot",
+            "show me how to get to $loc by foot",
+            "show how to get to $loc by walking",
+            "what's the quickest way to walk to $loc"
+        ).map {
+            testMapHelper(it, expectedMode = Maps.WALK_MODE)
+        }
+    }
+
+    @Test
+    fun testRecommendThingMatches() {
+        val thing = "froyo"
+        listOf(
+            "where to find $thing",
+            "where's the nearest $thing",
+            "where's the nearest place for $thing",
+            "find nearby $thing"
+        ).map {
+            testMapHelper(
+                it,
+                expectedThing = thing
+            )
+        }
+    }
+
+    @Test
+    fun testRecommendThingLocMatches() {
+        val thing = "soft serve ice cream"
+        listOf(
+            Pair("where can i buy $thing in LOC", "the mission"),
+            Pair("what's a good place for $thing near LOC", "the bay bridge"),
+            Pair("search for $thing in LOC", "san francisco")
+        ).map { pair ->
+            testMapHelper(
+                pair.first.replace("LOC", pair.second),
+                expectedThing = thing,
+                expectedLocation = pair.second
+            )
         }
     }
 
