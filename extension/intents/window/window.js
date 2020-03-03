2c3677dd1dbbb19ba306bf7465b08173c711978f
json <yuanyu90221@gmail.com>
2020-03-03 14:22:06 +0800

    fix style problem with npm run format && do some refactor with review

--- extension/intents/window/window.js
+++ extension/intents/window/window.js
@@ -1,42 +1,39 @@
 import * as intentRunner from "../../background/intentRunner.js";
 
 function findTargetWindowId(windowArray, currentWindowId, direction) {
-    const len = windowArray.length;
-    // find currentWindowId postion in array
-    const currentWindowIndex = windowArray.findIndex((window) => (window.id === currentWindowId));
-    let targetIndex = 0;
-    if ( direction === "next" ) {
-      targetIndex = Math.floor((currentWindowIndex + 1) % len);
-    } else {
-      targetIndex = Math.floor((currentWindowIndex - 1 + len) % len);
-    }
-    return windowArray[targetIndex].id;
+  const len = windowArray.length;
+  // find currentWindowId postion in array
+  const currentWindowIndex = windowArray.findIndex(
+    window => window.id === currentWindowId
+  );
+  let targetIndex;
+  if (direction === "next") {
+    targetIndex = (currentWindowIndex + 1) % len;
+  } else {
+    targetIndex = (currentWindowIndex - 1 + len) % len;
   }
-// error handle for
-function onError(error) {
-  // console.log(`Error: ${error}`);
+  return windowArray[targetIndex].id;
 }
 
 intentRunner.registerIntent({
-    name: "window.switch",
-    async run(context) {
-        // get current activeTab.windowId
-        const activeTab = await context.activeTab();
-        const currentWindowId = activeTab.windowId;
-        // get direction parameter
-        let direction = "next";
-        if ( context.parameters ) {
-           direction = context.parameters.direction;
-        }
-        try {
-          // getAll normal window
-          const gettingAll = await browser.windows.getAll({windowTypes: ["normal"]});
-          // find target windowId
-          const targetWindowId = findTargetWindowId(gettingAll, currentWindowId, direction);
-          // set target window focuse true
-          await browser.windows.update(targetWindowId, {focused: true});
-        } catch (err) {
-          onError(err);
-        }
-    }
-  });
+  name: "window.switch",
+  async run(context) {
+    // get current activeTab.windowId
+    const activeTab = await context.activeTab();
+    const currentWindowId = activeTab.windowId;
+    // get direction parameter
+    const direction = context.parameters.direction;
+    // getAll normal window
+    const gettingAll = await browser.windows.getAll({
+      windowTypes: ["normal"],
+    });
+    // find target windowId
+    const targetWindowId = findTargetWindowId(
+      gettingAll,
+      currentWindowId,
+      direction
+    );
+    // set target window focuse true
+    await browser.windows.update(targetWindowId, { focused: true });
+  },
+});
