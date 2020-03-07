8b6b3bf9e4bccac559e3e3aefbbd649d95cb3a58
manasa2850 <manasakashyap28@gmail.com>
2020-03-07 13:08:42 +0530

    submit form intent

--- extension/intents/forms/formsContentScript.js
+++ extension/intents/forms/formsContentScript.js
@@ -177,12 +177,11 @@ this.dictationContentScript = (function() {
   });
 
   communicate.register("formSubmit", async message => {
-    if(document.activeElement.form == NULL) {
+    if (document.activeElement.form === null) {
       const e = new Error("No input field has been focussed");
       e.displayMessage = "Please focus on an input field before trying to submit";
       throw e;
-    }
-    else {
+    } else {
       document.activeElement.form.submit();
     }
     return true;
