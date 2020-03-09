5e137f94c7be111ce3a61935a1a53947523cf738
MELVIN GEORGE <melvingeorge10@gmail.com>
2020-03-09 23:16:54 +0530

    npm run format - done

--- extension/services/soundcloud/soundcloud.js
+++ extension/services/soundcloud/soundcloud.js
@@ -4,71 +4,71 @@ import * as content from "../../background/content.js";
 import { shouldDisplayWarning } from "../../limiter.js";
 
 class Soundcloud extends serviceList.Service {
-    async playQuery(query) {
-        await this.initTab("/services/soundcloud/player.js");
-        await this.callTab("search", { query, thenPlay: true });
-        if (this.tabCreated) {
-          const isAudible = await this.pollTabAudible(this.tab.id, 3000);
-          if (!isAudible) {
-            const activeTabId = (await this.context.activeTab()).id;
-            this.context.makeTabActive(this.tab);
-            const nowAudible = await this.pollTabAudible(this.tab.id, 1000);
-            if (
-              nowAudible ||
-              !(await shouldDisplayWarning("soundcloudAudible", {
-                times: 3,
-                frequency: 1000,
-              }))
-            ) {
-              if (this.tab.id !== activeTabId) {
-                this.context.makeTabActive(activeTabId);
-              }
-            } else {
-              this.context.failedAutoplay(this.tab);
-            }
+  async playQuery(query) {
+    await this.initTab("/services/soundcloud/player.js");
+    await this.callTab("search", { query, thenPlay: true });
+    if (this.tabCreated) {
+      const isAudible = await this.pollTabAudible(this.tab.id, 3000);
+      if (!isAudible) {
+        const activeTabId = (await this.context.activeTab()).id;
+        this.context.makeTabActive(this.tab);
+        const nowAudible = await this.pollTabAudible(this.tab.id, 1000);
+        if (
+          nowAudible ||
+          !(await shouldDisplayWarning("soundcloudAudible", {
+            times: 3,
+            frequency: 1000,
+          }))
+        ) {
+          if (this.tab.id !== activeTabId) {
+            this.context.makeTabActive(activeTabId);
           }
+        } else {
+          this.context.failedAutoplay(this.tab);
         }
+      }
     }
+  }
 
-    async move(direction) {
-        const tabs = await this.getAllTabs();
-        if (!tabs.length) {
-          const e = new Error("Spotify is not open");
-          e.displayMessage = "Spotify is not open";
-          throw e;
-        }
-        for (const tab of tabs) {
-          await content.lazyInject(tab.id, "/services/soundcloud/player.js");
-          await this.callOneTab(tab.id, "move", { direction });
-        }
+  async move(direction) {
+    const tabs = await this.getAllTabs();
+    if (!tabs.length) {
+      const e = new Error("Spotify is not open");
+      e.displayMessage = "Spotify is not open";
+      throw e;
     }
+    for (const tab of tabs) {
+      await content.lazyInject(tab.id, "/services/soundcloud/player.js");
+      await this.callOneTab(tab.id, "move", { direction });
+    }
+  }
 
-    async pause() {
-        await this.initTab("/services/soundcloud/player.js");
-        await this.callTab("pause");
-    }
+  async pause() {
+    await this.initTab("/services/soundcloud/player.js");
+    await this.callTab("pause");
+  }
 
-    async unpause() {
-        await this.initTab("/services/soundcloud/player.js");
-        await this.callTab("unpause");
-    }
+  async unpause() {
+    await this.initTab("/services/soundcloud/player.js");
+    await this.callTab("unpause");
+  }
 
-    async pauseAny(options) {
-        const exceptTabId = options && options.exceptTabId;
-        for (const tab of await this.getAllTabs({ audible: true })) {
-          if (exceptTabId && exceptTabId === tab.id) {
-            continue;
-          }
-          await content.lazyInject(tab.id, "/services/soundcloud/player.js");
-          await this.callOneTab(tab.id, "pause");
-        }
+  async pauseAny(options) {
+    const exceptTabId = options && options.exceptTabId;
+    for (const tab of await this.getAllTabs({ audible: true })) {
+      if (exceptTabId && exceptTabId === tab.id) {
+        continue;
+      }
+      await content.lazyInject(tab.id, "/services/soundcloud/player.js");
+      await this.callOneTab(tab.id, "pause");
     }
+  }
 }
 
 Object.assign(Soundcloud, {
-    id: "soundcloud",
-    title: "SoundCloud",
-    baseUrl: "https://soundcloud.com/",
+  id: "soundcloud",
+  title: "SoundCloud",
+  baseUrl: "https://soundcloud.com/",
 });
 
 music.register(Soundcloud);
