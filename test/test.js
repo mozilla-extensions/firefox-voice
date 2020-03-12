c0af2297d84bf6dc91291142a5d94b19b000147a
Simpcyclassy <chioma.onyekpere@gmail.com>
2020-03-12 17:51:13 +0100

    Convert test/test.js to async/await

--- test/test.js
+++ test/test.js
@@ -3,34 +3,32 @@ const webdriver = require("selenium-webdriver");
 const { By, until, Builder } = webdriver;
 const path = require("path");
 const firefox = require("selenium-webdriver/firefox");
-const extension_manifest_path=path.join(process.cwd(),"extension","manifest.json")
-const version_name= require(extension_manifest_path).version
-const extension_name="firefox_voice_beta-"+version_name+".zip"
-const addonFileLocation = path.join(process.cwd(), "web-ext-artifacts",extension_name);
+const extension_manifest_path = path.join(process.cwd(), "extension", "manifest.json");
+const version_name = require(extension_manifest_path).version;
+const extension_name = "firefox_voice_beta-" + version_name + ".zip";
+const addonFileLocation = path.join(process.cwd(), "web-ext-artifacts", extension_name);
 (async function example() {
-
-  let options = new firefox.Options()
+  const options = new firefox.Options()
     .setPreference("extensions.legacy.enabled", true)
     .setPreference("xpinstall.signatures.required", false)
-    .setPreference("xpinstall.whitelist.required", false)
+    .setPreference("xpinstall.whitelist.required", false);
 
-  let driver = new Builder()
+  const driver = new Builder()
     .withCapabilities({ "moz:webdriverClick": true })
     .forBrowser("firefox")
     .setFirefoxOptions(options)
     .build();
 
 
- await driver.installAddon(addonFileLocation);
+  await driver.installAddon(addonFileLocation);
 
- await driver.setContext(firefox.Context.CHROME)
-.then(()=>
-driver.wait(until.elementLocated(By.id("pageActionButton"))))
-.then(()=>
-driver.wait(until.elementLocated(By.id("firefox-voice_mozilla_org-browser-action"))))
-.then(()=>console.log("Extension Present"))
-.catch((e)=>console.log(e))
+  await driver.setContext(firefox.Context.CHROME);
+
+  await driver.wait(until.elementLocated(By.id("pageActionButton")));
+
+  await driver.wait(until.elementLocated(By.id("firefox-voice_mozilla_org-browser-action")));
+
+  console.log("Extension Present");
 
   await driver.quit();
-
 })();
