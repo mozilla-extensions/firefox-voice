2e9646ca46532c94e12d3959325a5d10581670c2
ASHUTOSH KUMAR SINGH <ashutoshs.ic18@nsut.ac.in>
2020-03-12 18:28:21 +0530

    Create test.js

---
+++ test/test.js
@@ -0,0 +1,34 @@
+
+const webdriver = require("selenium-webdriver");
+const { By, until, Builder } = webdriver;
+const path = require("path");
+const firefox = require("selenium-webdriver/firefox");
+const addonFileLocation = path.join(process.cwd(), "web-ext-artifacts", "firefox_voice_beta-0.19.4502.zip");
+
+(async function example() {
+
+  let options = new firefox.Options()
+    .setPreference("extensions.legacy.enabled", true)
+    .setPreference("xpinstall.signatures.required", false)
+    .setPreference("xpinstall.whitelist.required", false)
+
+  let driver = new Builder()
+    .withCapabilities({ "moz:webdriverClick": true })
+    .forBrowser("firefox")
+    .setFirefoxOptions(options)
+    .build();
+
+
+ await driver.installAddon(addonFileLocation);
+
+ await driver.setContext(firefox.Context.CHROME)
+.then(()=>
+driver.wait(until.elementLocated(By.id("pageActionButton"))))
+.then(()=>
+driver.wait(until.elementLocated(By.id("firefox-voice_mozilla_org-browser-action"))))
+.then(()=>console.log("Extension Present"))
+.catch((e)=>console.log(e))
+
+  await driver.quit();
+
+})();
