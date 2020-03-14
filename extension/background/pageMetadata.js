5466d93fc3fc8eef18f5a7d69f39a9a4614f659b
Anna <annlinros@gmail.com>
2020-03-14 11:40:04 +0100

    Refactored cleanURL function

--- extension/background/pageMetadata.js
+++ extension/background/pageMetadata.js
@@ -10,16 +10,43 @@ export async function getSelection(tabId) {
 
 export async function getMetadata(tabId) {
   await content.lazyInject(tabId, "/background/pageMetadata-contentScript.js");
-  return browser.tabs.sendMessage(tabId, {
+  const metadata = await browser.tabs.sendMessage(tabId, {
     type: "getMetadata",
   });
+  metadata.canonical = cleanURL(metadata.canonical);
+  return metadata;
 }
 
-export async function cleanURL(url) {
-  await content.lazyInject(url, "/background/pageMetadata-contentScript.js");
-  browser.webRequest.onBeforeRequest.addListener(
-    cleanURL,
-    { urls: ["<all_urls>"] },
-    ["blocking"]
-  );
+function cleanURL(string) {
+  const globalBlockedParams = [
+    "utm_source",
+    "utm_medium",
+    "utm_campaign",
+    "utm_term",
+    "utm_content",
+  ];
+
+  function getReducedParams(str) {
+    const url = new URL(str);
+    const params = url.searchParams;
+
+    globalBlockedParams.map(blockedParam => {
+      if (params.get(blockedParam)) {
+        params.delete(blockedParam);
+      }
+    });
+    return params.toString();
+  }
+
+  function buildURL(baseURL, params) {
+    if (Object.keys(params).length === 0) {
+      return baseURL;
+    }
+    const newURL = baseURL + "?" + params;
+    return newURL;
+  }
+
+  const baseURL = string.split("?")[0];
+  const params = getReducedParams(string);
+  return buildURL(baseURL, params);
 }
