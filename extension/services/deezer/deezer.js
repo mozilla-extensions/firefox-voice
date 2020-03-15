c3622581569dd70c09873621cd1189338b090239
Bukola Odufade <bodufade@gmail.com>
2020-03-15 01:04:03 +0100

    created deezer as a music service

---
+++ extension/services/deezer/deezer.js
@@ -0,0 +1,15 @@
+
+import * as music from "../../intents/music/music.js";
+import MusicService from "../../background/musicService.js";
+
+class Deezer extends MusicService {}
+
+Object.assign(Deezer, {
+  id: "deezer",
+  title: "Deezer",
+  baseUrl: "https://deezer.com/",
+});
+
+music.register(Deezer);
+
+
