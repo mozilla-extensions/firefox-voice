7095878efb78ebe5cbcec9b40bc692303d2521c6
xlisachan <xlisachan@gmail.com>
2020-03-16 14:47:04 -0400

    Formatting - updated indentation

--- test/integration.test.js
+++ test/integration.test.js
@@ -9,22 +9,22 @@ const extension_name = "firefox_voice_beta-" + version_name + ".zip";
 const addonFileLocation = join(process.cwd(), "web-ext-artifacts", extension_name);
 
 describe("Build/install extension, and check that toolbar button is present", () => {
-    it("Toolbar button is present", async () => {
-        const options = new Options()
-            .setPreference("extensions.legacy.enabled", true)
-            .setPreference("xpinstall.signatures.required", false)
-            .setPreference("xpinstall.whitelist.required", false);
+  it("Toolbar button is present", async () => {
+    const options = new Options()
+      .setPreference("extensions.legacy.enabled", true)
+      .setPreference("xpinstall.signatures.required", false)
+      .setPreference("xpinstall.whitelist.required", false);
 
-        const driver = new Builder()
-            .withCapabilities({ "moz:webdriverClick": true })
-            .forBrowser("firefox")
-            .setFirefoxOptions(options)
-            .build();
+    const driver = new Builder()
+      .withCapabilities({ "moz:webdriverClick": true })
+      .forBrowser("firefox")
+      .setFirefoxOptions(options)
+      .build();
 
-        await driver.installAddon(addonFileLocation);
-        await driver.setContext(Context.CHROME);
-        await driver.wait(until.elementLocated(By.id("pageActionButton")));
-        await driver.wait(until.elementLocated(By.id("firefox-voice_mozilla_org-browser-action")));
-        await driver.quit();
-    }, 30000);
+    await driver.installAddon(addonFileLocation);
+    await driver.setContext(Context.CHROME);
+    await driver.wait(until.elementLocated(By.id("pageActionButton")));
+    await driver.wait(until.elementLocated(By.id("firefox-voice_mozilla_org-browser-action")));
+    await driver.quit();
+  }, 30000);
 });
