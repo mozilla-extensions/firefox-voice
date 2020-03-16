93de4d45343aa4f3f1bd29f5fd647f2611884a1c
Dave <callremz@gmail.com>
2020-03-16 12:07:31 +0100

    create slideshow intent with .toml file

---
+++ extension/intents/slideshow/slideshow.js
@@ -0,0 +1,24 @@
+import * as intentRunner from "../../background/intentRunner.js";
+import * as content from "../../background/content.js";
+
+const SLIDESHOW_SCRIPT = "/intents/slideshow/contentScript.js";
+const SLIDESHOW_CSS = "/intents/slideshow/contentScript.css";
+
+intentRunner.registerIntent({
+    name: "slideshow.open",
+    async run(context) {
+        const activeTab = await context.activeTab();
+        const activeTabId = activeTab.id;
+        await content.lazyInject(activeTabId, SLIDESHOW_SCRIPT);
+        await browser.tabs.insertCSS(activeTabId, {file: SLIDESHOW_CSS});
+        const success = await browser.tabs.sendMessage(activeTabId, {
+            type: "openSlide"
+        });
+
+        if (!success) {
+            const err = new Error("Could not open slideshow");
+            err.displayMessage = "Could not open slideshow";
+            throw err;
+        }
+    }
+});
