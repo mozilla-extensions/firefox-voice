60cbc5473be4f0349713a986f458934603201bfd
LilyLME <brilyjoy@gmail.com>
2020-03-20 13:07:29 +0100

    used await to in catching search bar to reduce failures
    Signed-off-by: LilyLME <brilyjoy@gmail.com>

--- extension/services/applemusic/player.js
+++ extension/services/applemusic/player.js
@@ -1,46 +1,61 @@
 /* globals helpers */
 
 this.player = (function() {
-    const SEARCH_PLAY = ".lookup__controls .play-button";
-  
-    class Player extends helpers.Runner {
-      action_play() {
-        const button = this.querySelector(SEARCH_PLAY);
-        button.click();
-      }
-  
-      async action_search({ query, thenPlay }) {
-        const searchInput = this.querySelector("#search-box .dt-search-box__input");
-        searchInput.focus();
-        this.setReactInputValue(searchInput, query);
-        searchInput.form.submit();
-        if (thenPlay) {
-          const playerButton = await this.waitForSelector(SEARCH_PLAY, {
-            timeout: 3000,
-          });
-          playerButton.click();
-        }
-      }
-  
-      action_pause() {
-        const playControlsBtn = this.querySelectorAll(".web-chrome-playback-controls__playback-btn");
-        playControlsBtn[1].click();
-      }
-  
-      action_unpause() {
-        const playControlsBtn = this.querySelectorAll(".web-chrome-playback-controls__playback-btn");
-        playControlsBtn[1].click();
-      }
-  
-      action_move({ direction }) {
-        const playControlsBtn = this.querySelectorAll(".web-chrome-playback-controls__playback-btn");
-        if (direction === "next") {
-          playControlsBtn[2].click();
-        } else if (direction === "back") {
-          playControlsBtn[0].click();
-          playControlsBtn[0].click();
-        }
+  const SEARCH_PLAY = ".lockup__controls .play-button";
+
+  class Player extends helpers.Runner {
+    action_play() {
+      const button = this.querySelector(SEARCH_PLAY);
+      button.click();
+    }
+
+    async action_search({ query, thenPlay }) {
+      const searchInput = await this.waitForSelector(
+        "#search-box .dt-search-box__input"
+      );
+      const pressEnter = new KeyboardEvent("keydown", {
+        bubbles: true,
+        cancelable: true,
+        keyCode: 13,
+      });
+      searchInput.onfocus = function() {
+        document.body.dispatchEvent(pressEnter);
+      };
+      this.setReactInputValue(searchInput, query);
+      searchInput.focus();
+      if (thenPlay) {
+        const playerButton = await this.waitForSelector(SEARCH_PLAY, {
+          timeout: 3000,
+        });
+        playerButton.click();
       }
     }
-    Player.register();
-  })();
+
+    action_pause() {
+      const playControlsBtn = this.querySelectorAll(
+        ".web-chrome-playback-controls__playback-btn"
+      );
+      playControlsBtn[1].click();
+    }
+
+    action_unpause() {
+      const playControlsBtn = this.querySelectorAll(
+        ".web-chrome-playback-controls__playback-btn"
+      );
+      playControlsBtn[1].click();
+    }
+
+    action_move({ direction }) {
+      const playControlsBtn = this.querySelectorAll(
+        ".web-chrome-playback-controls__playback-btn"
+      );
+      if (direction === "next") {
+        playControlsBtn[2].click();
+      } else if (direction === "back") {
+        playControlsBtn[0].click();
+        playControlsBtn[0].click();
+      }
+    }
+  }
+  Player.register();
+})();
