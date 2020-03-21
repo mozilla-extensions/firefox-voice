ee9cdd7e6ce92b4b05914f714c3b40ecbc26f624
JanviMahajan14 <janvimahajan337@gmail.com>
2020-03-21 09:51:44 +0530

    Run format

--- extension/intents/pocket/pocket.js
+++ extension/intents/pocket/pocket.js
@@ -3,28 +3,25 @@ import * as intentRunner from "../../background/intentRunner.js";
 import * as content from "../../background/content.js";
 
 intentRunner.registerIntent({
-    name: "pocket.open",
-    async run(context) {
-        const url = "https://app.getpocket.com/";
-        await context.createTab({ url });
-    }
+  name: "pocket.open",
+  async run(context) {
+    const url = "https://app.getpocket.com/";
+    await context.createTab({ url });
+  },
 });
 
 intentRunner.registerIntent({
-    name: "pocket.save",
-    async run(context) {
-      const activeTab = await context.activeTab();
-      const metadata = await pageMetadata.getMetadata(activeTab.id);
-      await content.lazyInject(activeTab.id, [
-        "/background/pageMetadata-contentScript.js",
-        "/intents/pocket/pocketContentScript.js"
-      ]);
-      await browser.tabs.sendMessage(activeTab.id, {
-        type: "save",
-        url: metadata.url,
-      });
-    },
+  name: "pocket.save",
+  async run(context) {
+    const activeTab = await context.activeTab();
+    const metadata = await pageMetadata.getMetadata(activeTab.id);
+    await content.lazyInject(activeTab.id, [
+      "/background/pageMetadata-contentScript.js",
+      "/intents/pocket/pocketContentScript.js",
+    ]);
+    await browser.tabs.sendMessage(activeTab.id, {
+      type: "save",
+      url: metadata.url,
+    });
+  },
 });
-
-
-
--- extension/intents/pocket/pocketContentScript.js
+++ extension/intents/pocket/pocketContentScript.js
@@ -1,10 +1,10 @@
 /* globals communicate */
 
 this.pocketContentScript = (function() {
-    communicate.register("save", async message => {
-        const url = message.url;
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", "https://getpocket.com/save?url=" + url, true);
-        xhr.send();
-    });
+  communicate.register("save", async message => {
+    const url = message.url;
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://getpocket.com/save?url=" + url, true);
+    xhr.send();
+  });
 })();
