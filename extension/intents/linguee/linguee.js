355a1c320fbbf96f72319fb334eef9b650a6b863
Anna <annlinros@gmail.com>
2020-03-30 18:03:52 +0200

    search and say functionality for linguee

---
+++ extension/intents/linguee/linguee.js
@@ -0,0 +1,35 @@
+/* globals log */
+
+import * as intentRunner from "../../background/intentRunner.js";
+import * as searching from "../../searching.js";
+import * as content from "../../background/content.js";
+
+intentRunner.registerIntent({
+  name: "linguee.say",
+  async run(context) {
+    const myurl = await searching.ddgBangSearchUrl(
+      context.slots.query,
+      "linguee"
+    );
+
+    const lingueeTab = await context.createTab({ url: myurl });
+
+    const search = await content.lazyInject(lingueeTab.id, [
+      "/intents/linguee/contentScript.js",
+    ]);
+    log.info(search);
+    await browser.tabs.sendMessage(lingueeTab.id, { type: "sayWord" });
+
+    /*  window.addEventListener("load", event => {
+      const button = document.querySelectorAll("audio");
+      log.info(button[0]);
+    }); */
+
+    /*     if (!button[0]) {
+      const e = new Error(`Could not find audio for ${button}`);
+      e.name = "AudioNotFound";
+      throw e;
+    }
+    button[0].click(); */
+  },
+});
