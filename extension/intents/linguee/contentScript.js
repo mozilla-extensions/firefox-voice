d774303d0f6dca0ea404cf3f3873991425d6c9fd
Anna <annlinros@gmail.com>
2020-03-30 18:03:52 +0200

    search and say functionality for linguee

---
+++ extension/intents/linguee/contentScript.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+
+this.contentScript = (function() {
+  communicate.register("sayWord", async message => {
+    const button = document.querySelectorAll("audio");
+    log.info(button);
+    button[0].click();
+  });
+})();
