aad8393bc914c7eadf8446535a90251bc6e42d50
Anna <annlinros@gmail.com>
2020-04-02 15:42:15 +0200

    updated code

--- extension/intents/linguee/contentScript.js
+++ extension/intents/linguee/contentScript.js
@@ -2,7 +2,7 @@
 
 this.contentScript = (function() {
   communicate.register("sayWord", async message => {
-    const button = document.querySelectorAll("audio");
+    const button = document.querySelectorAll(".audio");
     log.info(button);
     button[0].click();
   });
