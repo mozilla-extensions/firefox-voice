2d1ae2497a33558768bb5af879db95cbc1086817
Simpcyclassy <chioma.onyekpere@gmail.com>
2020-04-06 20:59:38 +0100

    add missing comma

--- extension/experiments/voice/api.js
+++ extension/experiments/voice/api.js
@@ -73,7 +73,7 @@ this.voice = class extends ExtensionAPI {
 
           async showAllHistory() {
             return runCommand("Browser:ShowAllHistory");
-          }
+          },
         },
       },
     };
