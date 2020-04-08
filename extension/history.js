78b6dd951f178e12d482588226d55e6de419cc24
Michael <mikel.liu@mail.utoronto.ca>
2020-04-07 21:12:26 -0400

    change static to prototype function

--- extension/history.js
+++ extension/history.js
@@ -1,9 +1,6 @@
 /* globals log */
 
 export class Database {
-  static readonly = "readonly";
-  static readwrite = "readwrite";
-
   constructor(dbName) {
     this.dbName = dbName;
   }
@@ -28,79 +25,18 @@ export class Database {
     });
   }
 
-  static get(dbName, tbName, primaryKey) {
-    return new Promise((resolve, reject) => {
-      const request = indexedDB.open(dbName);
-      request.onsuccess = e => {
-        const database = e.target.result;
-        const transaction = database.transaction([tbName]);
-        const objectStore = transaction.objectStore(tbName);
-        const read = objectStore.get(primaryKey);
-        read.onsuccess = e => {
-          log.info("Got result:", read.result);
-          resolve(read.result);
-        };
-        read.onerror = e => {
-          const code = e.target.errorCode;
-          reject(new Error(`Unable to retrieve data from database: ${code}`));
-        };
-        database.close();
-      };
-      request.onerror = e => {
-        const code = e.target.errorCode;
-        reject(new Error(`Unable to retrieve from database: ${code}`));
-      };
-    });
-  }
-
-  /**
-   * Retrieves all documents from the table.
-   * @param {string} tbName - the table name to retrieve documents from
-   * @param {string} direction - "prev" is most recent first (based on
-   * primaryKey), "next" is most recent last (based on primaryKey)
-   * @returns {Promise} The documents
-   */
-  static getAll(dbName, tbName, direction) {
-    return new Promise((resolve, reject) => {
-      const request = indexedDB.open(dbName);
-      request.onsuccess = e => {
-        const database = e.target.result;
-        const objectStore = database.transaction(tbName).objectStore(tbName);
-        const list = [];
-        // default to descending order (or most recent if
-        // the primary key is timestamp)
-        const request = direction
-          ? objectStore.openCursor(null, direction)
-          : objectStore.openCursor(null, "prev");
-        request.onsuccess = e => {
-          const cursor = e.target.result;
-          if (cursor) {
-            list.push(cursor.value);
-            cursor.continue();
-          } else {
-            resolve(list);
-          }
-        };
-        request.onerror = e => {
-          const code = e.target.errorCode;
-          reject(new Error(`Unable to retrieve data from database: ${code}`));
-        };
-      };
-    });
-  }
-
   add(tbName, obj) {
     return new Promise((resolve, reject) => {
       const request = indexedDB.open(this.dbName);
       request.onsuccess = e => {
         const database = e.target.result;
         const add = database
-          .transaction([tbName], Database.readwrite)
+          .transaction([tbName], Database.prototype.readwrite)
           .objectStore(tbName)
-          .add();
+          .add(obj);
         add.onsuccess = e => {
           log.info("1 record has been added to your database.");
-          resolve(obj);
+          resolve();
         };
         add.onerror = e => {
           const code = e.target.errorCode;
@@ -110,48 +46,112 @@ export class Database {
       };
     });
   }
-
-  static delete(dbName, tbName, primaryKey) {
-    return new Promise((resolve, reject) => {
-      const request = indexedDB.open(dbName);
-      request.onsuccess = e => {
-        const database = e.target.result;
-        const remove = database
-          .transaction([tbName], Database.readwrite)
-          .objectStore(tbName)
-          .delete(primaryKey);
-        remove.onsuccess = e => {
-          log.info("This entry has been removed from your database.");
-          resolve();
-        };
-        remove.onerror = e => {
-          const code = e.target.errorCode;
-          reject(new Error(`Entry could not be removed in databse: ${code}`));
-        };
-        database.close();
-      };
-    });
-  }
-
-  static clearAll(dbName, tbName) {
-    return new Promise((resolve, reject) => {
-      const request = indexedDB.open(dbName);
-      request.onsuccess = e => {
-        const database = e.target.result;
-        const objectStore = database
-          .transaction(tbName, Database.readwrite)
-          .objectStore(tbName);
-        const clear = objectStore.clear();
-        clear.onsuccess = e => {
-          log.info("Successfully removed all entries in the table");
-          resolve();
-        };
-        clear.onerror = e => {
-          const code = e.target.errorCode;
-          reject(new Error(`Failed to remove entries in database: ${code}`));
-        };
-        database.close();
-      };
-    });
-  }
 }
+
+Database.prototype.readonly = "readonly";
+Database.prototype.readwrite = "readwrite";
+
+Database.get = (dbName, tbName, primaryKey) => {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(dbName);
+    request.onsuccess = e => {
+      const database = e.target.result;
+      const transaction = database.transaction([tbName]);
+      const objectStore = transaction.objectStore(tbName);
+      const read = objectStore.get(primaryKey);
+      read.onsuccess = e => {
+        log.info("Got result:", read.result);
+        resolve(read.result);
+      };
+      read.onerror = e => {
+        const code = e.target.errorCode;
+        reject(new Error(`Unable to retrieve data from database: ${code}`));
+      };
+      database.close();
+    };
+    request.onerror = e => {
+      const code = e.target.errorCode;
+      reject(new Error(`Unable to retrieve from database: ${code}`));
+    };
+  });
+};
+
+/**
+ * Retrieves all documents from the table.
+ * @param {string} tbName - the table name to retrieve documents from
+ * @param {string} direction - "prev" is most recent first (based on
+ * primaryKey), "next" is most recent last (based on primaryKey)
+ * @returns {Promise} The documents
+ */
+Database.getAll = (dbName, tbName, direction) => {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(dbName);
+    request.onsuccess = e => {
+      const database = e.target.result;
+      const objectStore = database.transaction(tbName).objectStore(tbName);
+      const list = [];
+      // default to descending order (or most recent if
+      // the primary key is timestamp)
+      const request = direction
+        ? objectStore.openCursor(null, direction)
+        : objectStore.openCursor(null, "prev");
+      request.onsuccess = e => {
+        const cursor = e.target.result;
+        if (cursor) {
+          list.push(cursor.value);
+          cursor.continue();
+        } else {
+          resolve(list);
+        }
+      };
+      request.onerror = e => {
+        const code = e.target.errorCode;
+        reject(new Error(`Unable to retrieve data from database: ${code}`));
+      };
+    };
+  });
+};
+
+Database.delete = (dbName, tbName, primaryKey) => {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(dbName);
+    request.onsuccess = e => {
+      const database = e.target.result;
+      const remove = database
+        .transaction([tbName], Database.prototype.readwrite)
+        .objectStore(tbName)
+        .delete(primaryKey);
+      remove.onsuccess = e => {
+        log.info("This entry has been removed from your database.");
+        resolve();
+      };
+      remove.onerror = e => {
+        const code = e.target.errorCode;
+        reject(new Error(`Entry could not be removed in databse: ${code}`));
+      };
+      database.close();
+    };
+  });
+};
+
+Database.clearAll = (dbName, tbName) => {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(dbName);
+    request.onsuccess = e => {
+      const database = e.target.result;
+      const objectStore = database
+        .transaction(tbName, Database.prototype.readwrite)
+        .objectStore(tbName);
+      const clear = objectStore.clear();
+      clear.onsuccess = e => {
+        log.info("Successfully removed all entries in the table");
+        resolve();
+      };
+      clear.onerror = e => {
+        const code = e.target.errorCode;
+        reject(new Error(`Failed to remove entries in database: ${code}`));
+      };
+      database.close();
+    };
+  });
+};
