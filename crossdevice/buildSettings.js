bc3d064ad7d0fd6bfb2afff36a605becba8e0456
Ian Bicking <ian@ianbicking.org>
2020-04-15 12:52:20 -0500

    Add experimental remote control

---
+++ crossdevice/buildSettings.js
@@ -0,0 +1,25 @@
+this.buildSettings = {
+  logLevel: "debug",
+  inDevelopment: false,
+  sttServer: "https://speaktome-2.services.mozilla.com",
+  deepSpeechServer: "https://dev.speaktome.nonprod.cloudops.mozgcp.net",
+  sendToDeepSpeech: true,
+  gitCommit: "29bcff6-dirty",
+  buildTime: "2020-04-15T17:47:15.993Z",
+  backgroundTabRecorder: false,
+  channel: "unknown",
+  sentryDsn: "https://18143ece2d6042ff91ed7678b287a814@sentry.prod.mozaws.net/458",
+  openPopupOnStart: false,
+  android: false,
+  // Note: this should only be enabled in demo situations:
+  executeIntentUrl: null,
+  firebaseConfig: {
+    apiKey: "AIzaSyDh5CJWiog4lXde47YPwPffSh1XMLF4-KE",
+    authDomain: "firefox-voice.firebaseapp.com",
+    databaseURL: "https://firefox-voice.firebaseio.com",
+    projectId: "firefox-voice",
+    storageBucket: "firefox-voice.appspot.com",
+    messagingSenderId: "366523679149",
+    appId: "1:366523679149:web:63fcb6f3aa624af6c5e124"
+  },
+};
