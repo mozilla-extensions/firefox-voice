mozilla-extensions/firefox-voice

commit 1b38e35c799b8808f8f728192ec914bd1df24f33

Allow followup questions (#1296)
This PR will resolve #962 which allows listening for follow up questions.
Requirements
Next intent/phrase directly associated with previous intent

- user says "search for x"
- show search results and listen for follow up
- user says "next result"
- the calling intent handles the follow up

extension/popup/popupView.jsx
@@ -4,7 +4,7 @@
 
 import * as browserUtil from "../browserUtil.js";
 
-const { Component, useState, useEffect, PureComponent } = React;
+const { useState, useEffect, PureComponent } = React;
 
 export const Popup = ({
   currentView,
@@ -29,6 +29,8 @@ export const Popup = ({
   expandListeningView,
   timerInMS,
   timerTotalInMS,
+  renderFollowup,
+  followupText,
 }) => {
   const [inputValue, setInputValue] = useState(null);
   function savingOnInputStarted(value) {
@@ -38,7 +40,10 @@ export const Popup = ({
     onInputStarted();
   }
   return (
-    <div id="popup" className={currentView}>
+    <div
+      id="popup"
+      className={`${currentView} ${renderFollowup ? "followup" : ""}`}
+    >
       <PopupHeader
         currentView={currentView}
         transcript={transcript}
@@ -66,9 +71,11 @@ export const Popup = ({
         expandListeningView={expandListeningView}
         timerInMS={timerInMS}
         timerTotalInMS={timerTotalInMS}
+        renderFollowup={renderFollowup}
       />
       <PopupFooter
         currentView={currentView}
+        renderFollowup={renderFollowup}
         showSettings={showSettings}
         timerInMS={timerInMS}
       />
@@ -78,6 +85,12 @@ export const Popup = ({
           timerInMS={timerInMS}
         ></TimerFooter>
       ) : null}
+      <FollowupContainer
+        lastIntent={lastIntent}
+        followupText={followupText}
+        renderFollowup={renderFollowup}
+        currentView={currentView}
+      />
     </div>
   );
 };
@@ -195,6 +208,7 @@ const PopupContent = ({
   expandListeningView,
   timerInMS,
   timerTotalInMS,
+  renderFollowup,
 }) => {
   const getContent = () => {
     switch (currentView) {
@@ -221,6 +235,7 @@ const PopupContent = ({
           <ProcessingContent
             transcript={transcript}
             displayText={displayText}
+            renderFollowup={renderFollowup}
           />
         );
       case "success":
@@ -245,6 +260,7 @@ const PopupContent = ({
             onNextSearchResultClick={onNextSearchResultClick}
             setMinPopupSize={setMinPopupSize}
             onSubmitFeedback={onSubmitFeedback}
+            renderFollowup={renderFollowup}
           />
         );
       case "startSavingPage":
@@ -276,6 +292,36 @@ const PopupContent = ({
     <div id="popup-content">
       <Zap currentView={currentView} recorderVolume={recorderVolume} />
       {getContent()}
+    </div>
+  );
+};
+
+const FollowupContainer = ({ followupText, renderFollowup, currentView }) => {
+  if (
+    currentView === "listening" ||
+    currentView === "waiting" ||
+    currentView === "processing" ||
+    !renderFollowup
+  ) {
+    return null;
+  }
+
+  let heading = "Waiting...";
+  let subheading;
+  if (followupText) {
+    heading = followupText.heading;
+    subheading = followupText.subheading;
+  }
+  return (
+    <div id="followup-container">
+      <IntentFeedback />
+      <div id="followup-wrapper">
+        <div id="followup_mic-container">Mic On</div>
+        <div id="followup_headings-wrapper">
+          <div id="followup_heading">{heading}</div>
+          {subheading && <div id="followup_subheading">{subheading}</div>}
+        </div>
+      </div>
     </div>
   );
 };
@@ -327,12 +373,13 @@ const FeedbackThanks = () => {
   );
 };
 
-const PopupFooter = ({ currentView, showSettings, timerInMS }) => {
+const PopupFooter = ({ currentView, showSettings, renderFollowup }) => {
   if (
     currentView === "searchResults" ||
     currentView === "feedback" ||
     currentView === "feedbackThanks" ||
-    currentView === "timer"
+    currentView === "timer" ||
+    renderFollowup
   )
     return null;
   return (
@@ -627,11 +674,11 @@ class TypingInput extends PureComponent {
   }
 }
 
-const ProcessingContent = ({ transcript, displayText }) => {
+const ProcessingContent = ({ transcript, displayText, renderFollowup }) => {
   return (
     <React.Fragment>
-      <Transcript transcript={transcript} />
-      <TextDisplay displayText={displayText} />
+      <Transcript transcript={renderFollowup ? "" : transcript} />
+      <TextDisplay displayText={renderFollowup ? "" : displayText} />
     </React.Fragment>
   );
 };
@@ -678,6 +725,7 @@ const SearchResultsContent = ({
   onNextSearchResultClick,
   setMinPopupSize,
   onSubmitFeedback,
+  renderFollowup,
 }) => {
   if (!search) return null;
 
@@ -741,29 +789,33 @@ const SearchResultsContent = ({
   return (
     <React.Fragment>
       <TextDisplay displayText={displayText} />
-      <div id="search-results">{renderCard()}</div>
-      <div id="search-footer">
-        <IntentFeedback
-          onSubmitFeedback={onSubmitFeedback}
-          eduText={card && card.answer ? card.answer.eduText : null}
-        />
-        {next ? (
-          <div id="next-result">
-            <p>
-              <strong>
-                Click mic and say <i>'next'</i> to view
-              </strong>
-            </p>
-            <a
-              href={next.url}
-              id="search-show-next"
-              onClick={onNextResultClick}
-            >
-              {new URL(next.url).hostname} | {next.title}
-            </a>
+      {renderFollowup ? null : (
+        <React.Fragment>
+          <div id="search-results">{renderCard()}</div>
+          <div id="search-footer">
+            <IntentFeedback
+              onSubmitFeedback={onSubmitFeedback}
+              eduText={card && card.answer ? card.answer.eduText : null}
+            />
+            {next ? (
+              <div id="next-result">
+                <p>
+                  <strong>
+                    Click mic and say <i>'next'</i> to view
+                  </strong>
+                </p>
+                <a
+                  href={next.url}
+                  id="search-show-next"
+                  onClick={onNextResultClick}
+                >
+                  {new URL(next.url).hostname} | {next.title}
+                </a>
+              </div>
+            ) : null}
           </div>
-        ) : null}
-      </div>
+        </React.Fragment>
+      )}
     </React.Fragment>
   );
 };
@@ -776,7 +828,7 @@ const TextDisplay = ({ displayText }) => {
   return displayText ? <div id="text-display">{displayText}</div> : null;
 };
 
-class Zap extends Component {
+class Zap extends PureComponent {
   constructor(props) {
     super(props);
 

extension/assets/css/gui.css
@@ -56,6 +56,10 @@ Example markup to use this styled native checkbox
   position: relative;
 }
 
+.checkbox-wrapper + .checkbox-wrapper {
+  padding-top: 12px;
+}
+
 .styled-checkbox label,
 .styled-toggleswitch label {
   padding-left: 40px;

extension/background/intentRunner.js
@@ -9,6 +9,7 @@ import { compile, splitPhraseLines } from "./language/compiler.js";
 import { metadata } from "../intents/metadata.js";
 import { entityTypes } from "./entityTypes.js";
 import { Database } from "../history.js";
+import * as settings from "../settings.js";
 const FEEDBACK_INTENT_TIME_LIMIT = 1000 * 60 * 60 * 24; // 24 hours
 // Only keep this many previous intents:
 const INTENT_HISTORY_LIMIT = 20;
@@ -17,10 +18,15 @@ const METADATA_ATTRIBUTES = new Set([
   "example",
   "examples",
   "match",
+  "followup",
 ]);
 
 export const intents = {};
 let lastIntent;
+// This is a copy of lastIntent that gets nullified when the pop up
+// window closes.
+let lastIntentForFollowup;
+let globalListenForFollowup = settings.getSettings().listenForFollowup;
 const intentHistory = [];
 const db = new Database("voice");
 const utteranceTable = "utterance";
@@ -30,10 +36,25 @@ db.createTable(utteranceTable, primaryKey, voiceVersion)
   .then(result => log.info("CREATE TABLE:", result))
   .catch(error => log.error(error));
 
+settings.watch("listenForFollowup", ns => {
+  globalListenForFollowup = ns;
+});
+
 export class IntentContext {
   constructor(desc) {
     this.closePopupOnFinish = true;
     this.timestamp = Date.now();
+    this.expectsFollowup = false;
+    this.isFollowup = false;
+    this.isFollowupIntent = false;
+    this.insistOnFollowup = false;
+    // allows us to reuse an intent's text on retries
+    this.followupHint = {};
+    // When running follow ups, sometimes a success view is not needed,
+    // yet still flashes before the next view. This attribute allows us to
+    // force bypassing the success view.
+    this.skipSuccessView = false;
+    this.acceptFollowupIntent = [];
     Object.assign(this, desc);
   }
 
@@ -50,7 +71,7 @@ export class IntentContext {
 
   done(time = undefined) {
     this.closePopupOnFinish = false;
-    if (!this.noPopup) {
+    if (!this.noPopup && !this.isFollowup && !this.isFollowupIntent) {
       return browser.runtime.sendMessage({
         type: "closePopup",
         time,
@@ -60,6 +81,7 @@ export class IntentContext {
   }
 
   failed(message) {
+    this.skipSuccessView = true;
     telemetry.add({ intentSuccess: false });
     telemetry.sendSoon();
     try {
@@ -87,6 +109,68 @@ export class IntentContext {
       type: "endSavingPage",
       message,
     });
+  }
+
+  async startFollowup(message = undefined) {
+    this.expectsFollowup = true;
+    if (message) {
+      this.acceptFollowupIntent = message.acceptFollowupIntent || [];
+      this.skipSuccessView = message.skipSuccessView || false;
+      this.insistOnFollowup = message.insistOnFollowup || false;
+      this.followupHint = {
+        heading: message.heading,
+        subheading: message.subheading,
+      };
+    }
+    return browser.runtime.sendMessage({
+      type: "handleFollowup",
+      method: "enable",
+      message: {
+        heading: this.followupHint.heading,
+        subheading: this.followupHint.subheading,
+      },
+    });
+  }
+
+  async endFollowup() {
+    this.expectsFollowup = false;
+    browser.runtime.sendMessage({
+      type: "handleFollowup",
+      method: "disable",
+    });
+  }
+
+  parseFollowup(utterance) {
+    if (!lastIntentForFollowup || !lastIntentForFollowup.followupMatch) {
+      return null;
+    }
+    if (!this.followupPhraseSet) {
+      const phrases = [];
+      for (const line of splitPhraseLines(
+        lastIntentForFollowup.followupMatch
+      )) {
+        const compiled = compile(line, {
+          entities: entityTypes,
+          intentName: lastIntentForFollowup.name,
+        });
+        phrases.push(compiled);
+      }
+      this.followupPhraseSet = new PhraseSet(phrases);
+    }
+
+    const result = this.followupPhraseSet.match(utterance);
+    if (!result) {
+      return null;
+    }
+
+    return {
+      name: result.intentName,
+      slots: result.stringSlots(),
+      parameters: result.parameters,
+      utterance,
+      fallback: false,
+      isFollowup: true,
+    };
   }
 
   displayText(message) {
@@ -245,6 +329,10 @@ export function registerIntent(intent) {
       );
     }
   }
+  if (data.followup) {
+    data.followupMatch = data.followup.match;
+    delete data.followup;
+  }
   Object.assign(intent, data);
   intents[intent.name] = intent;
   if (!intent.match) {
@@ -276,8 +364,32 @@ export async function runUtterance(utterance, noPopup) {
       }
     }
   }
-  const desc = intentParser.parse(utterance);
+  let desc = intentParser.parse(utterance);
   desc.noPopup = !!noPopup;
+  desc.followupMatch = intents[desc.name].followupMatch;
+  if (lastIntentForFollowup && lastIntentForFollowup.expectsFollowup) {
+    const followup = lastIntentForFollowup.parseFollowup(utterance);
+    if (followup) {
+      desc = followup;
+    } else if (
+      lastIntentForFollowup.acceptFollowupIntent &&
+      lastIntentForFollowup.acceptFollowupIntent.includes(desc.name)
+    ) {
+      desc.isFollowupIntent = true;
+    } else if (
+      !globalListenForFollowup ||
+      (globalListenForFollowup && lastIntentForFollowup.insistOnFollowup)
+    ) {
+      lastIntentForFollowup.failed(
+        `The phrase\n\n"${desc.utterance}"\n\nis invalid. Please try again`
+      );
+      return lastIntentForFollowup.startFollowup();
+    } else {
+      // This is a new intent when listening for all follow ups, so we
+      // clear previous follow up hints
+      lastIntentForFollowup.endFollowup();
+    }
+  }
   return runIntent(desc);
 }
 
@@ -289,6 +401,7 @@ export async function runIntent(desc) {
   const intent = intents[desc.name];
   const context = new IntentContext(desc);
   lastIntent = context;
+  lastIntentForFollowup = lastIntent;
   addIntentHistory(context);
   context.initTelemetry();
   try {
@@ -301,7 +414,11 @@ export async function runIntent(desc) {
         ? `and parameters: ${JSON.stringify(desc.parameters)}`
         : "and no params"
     );
-    await intent.run(context);
+    if (lastIntent.isFollowup) {
+      await intent.runFollowup(context);
+    } else {
+      await intent.run(context);
+    }
     if (context.closePopupOnFinish) {
       context.done();
     }
@@ -417,6 +534,12 @@ export function getLastIntentForFeedback() {
 
 export function clearFeedbackIntent() {
   lastIntent = null;
+}
+
+export function clearFollowup() {
+  if (lastIntentForFollowup) {
+    lastIntentForFollowup = null;
+  }
 }
 
 initRegisteredNicknames();

extension/background/main.js
@@ -108,6 +108,8 @@ browser.runtime.onMessage.addListener(async (message, sender) => {
     return new intentRunner.IntentContext(
       intentParser.parse(message.utterance, message.disableFallback)
     );
+  } else if (message.type === "clearFollowup") {
+    return intentRunner.clearFollowup();
   }
   log.error(
     `Received message with unexpected type (${message.type}): ${message}`

extension/intents/bookmarks/bookmarks.js
@@ -76,20 +76,38 @@ intentRunner.registerIntent({
 intentRunner.registerIntent({
   name: "bookmarks.remove",
   async run(context) {
-    const activeTab = await context.activeTab();
-    const metadata = await pageMetadata.getMetadata(activeTab.id);
-    const bookmarks = await browser.bookmarks.search({ url: metadata.url });
+    await context.displayText("Do you wish to remove this bookmark?");
+    await context.startFollowup({
+      heading: 'Say "YES" or "CANCEL"',
+      insistOnFollowup: true,
+    });
+  },
+  async runFollowup(context) {
+    switch (context.parameters.confirmation) {
+      case "false":
+        await context.displayText("Command cancelled");
+        await context.endFollowup();
+        break;
+      default:
+        const activeTab = await context.activeTab();
+        const metadata = await pageMetadata.getMetadata(activeTab.id);
+        const bookmarks = await browser.bookmarks.search({ url: metadata.url });
 
-    const selected = bookmarks.filter(
-      bookmark => activeTab.url === bookmark.url
-    );
+        const selected = bookmarks.filter(
+          bookmark => activeTab.url === bookmark.url
+        );
 
-    if (!selected.length) {
-      const e = new Error("This page wasn't bookmarked");
-      e.displayMessage = "This page wasn't bookmarked";
-      throw e;
+        if (!selected.length) {
+          context.displayText("");
+          context.endFollowup();
+          const e = new Error("This page wasn't bookmarked");
+          e.displayMessage = "This page wasn't bookmarked";
+          throw e;
+        }
+
+        await browser.bookmarks.remove(selected[0].id);
+        await context.displayText("Bookmark has been removed");
+        await context.endFollowup();
     }
-
-    await browser.bookmarks.remove(selected[0].id);
   },
 });

extension/intents/bookmarks/bookmarks.toml
@@ -49,6 +49,13 @@ match = """
   (remove | delete) (this |) (page | site | tab |) from bookmark{s}
 """
 
+[bookmarks.remove.followup]
+description = "Confirm that the user wishes to remove a bookmark"
+match = """
+   yes      [confirmation=true]
+   cancel   [confirmation=false]
+"""
+
 [[bookmarks.remove.example]]
 phrase = "Remove bookmark"
 test = true

extension/intents/search/search.js
@@ -223,6 +223,18 @@ async function moveResult(context, step) {
     // reset the index to an initial search result
     searchInfo.index = undefined;
     tabSearchResults.set(tabId, searchInfo);
+    await browser.runtime.sendMessage({
+      type: "showSearchResults",
+      searchResults: searchInfo.searchResults,
+      searchUrl: searchInfo.searchUrl,
+      index: 0,
+    });
+    context.startFollowup({
+      heading: "Say 'next' to view",
+      subheading: searchInfo.searchResults[0].title,
+      acceptFollowupIntent: ["search.next", "search.previous"],
+      skipSuccessView: true,
+    });
     return;
   }
 
@@ -232,12 +244,6 @@ async function moveResult(context, step) {
     searchInfo.index === undefined ? 0 : searchInfo.index + step;
 
   const item = searchInfo.searchResults[searchInfo.index];
-  await browser.runtime.sendMessage({
-    type: "showSearchResults",
-    searchResults: searchInfo.searchResults,
-    searchUrl: searchInfo.searchUrl,
-    index: searchInfo.index,
-  });
   if (!tabId) {
     const tab = await context.createTab({ url: item.url });
     // eslint-disable-next-line require-atomic-updates
@@ -267,6 +273,21 @@ async function moveResult(context, step) {
       tabSearchResults.delete(tabId);
     }
   }
+  await browserUtil.waitForDocumentComplete(lastTabId);
+  await browser.runtime.sendMessage({
+    type: "showSearchResults",
+    searchResults: searchInfo.searchResults,
+    searchUrl: searchInfo.searchUrl,
+    index: searchInfo.index,
+  });
+  const nextItemIdx = step === 1 ? searchInfo.index + 1 : searchInfo.index - 1;
+  const nextItem = searchInfo.searchResults[nextItemIdx];
+  context.startFollowup({
+    heading: `Say '${step === 1 ? "next" : "previous"}' to view`,
+    subheading: nextItemIdx < 0 ? "Search Results" : nextItem.title,
+    acceptFollowupIntent: ["search.next", "search.previous"],
+    skipSuccessView: true,
+  });
 }
 
 export async function focusSearchResults(message) {
@@ -406,6 +427,7 @@ intentRunner.registerIntent({
       });
 
       await focusSearchTab();
+      await browserUtil.waitForDocumentComplete(tabId);
       await content.lazyInject(tabId, "/intents/search/queryScript.js");
       const searchInfo = await callScript({ type: "searchResultInfo" });
 
@@ -421,6 +443,13 @@ intentRunner.registerIntent({
       }
 
       tabSearchResults.set(tabId, searchInfo);
+
+      context.keepPopup();
+      await context.startFollowup({
+        heading: "Say 'next' to view",
+        subheading: searchInfo.searchResults[0].title,
+        acceptFollowupIntent: ["search.next", "search.previous"],
+      });
     }
   },
 });

extension/options/optionsView.jsx
@@ -108,7 +108,7 @@ const General = ({
 }) => {
   return (
     <div className="settings-content">
-      <ChimeSettings
+      <PreferenceSettings
         userSettings={userSettings}
         updateUserSettings={updateUserSettings}
       />
@@ -169,24 +169,39 @@ const MusicServiceSettings = ({
   );
 };
 
-const ChimeSettings = ({ userSettings, updateUserSettings }) => {
-  const onChimeSettingChange = event => {
+const PreferenceSettings = ({ userSettings, updateUserSettings }) => {
+  const onPreferenceChange = setting => event => {
     if (event) {
-      userSettings.chime = event.target.checked;
+      userSettings[setting] = event.target.checked;
       updateUserSettings(userSettings);
     }
   };
   return (
     <fieldset id="preferences">
       <legend>Preferences</legend>
-      <div className="styled-checkbox">
-        <input
-          id="chime"
-          type="checkbox"
-          checked={userSettings.chime}
-          onChange={onChimeSettingChange}
-        />
-        <label htmlFor="chime">Play chime when opening mic</label>
+      <div className="checkbox-wrapper">
+        <div className="styled-checkbox">
+          <input
+            id="chime"
+            type="checkbox"
+            checked={userSettings.chime}
+            onChange={onPreferenceChange("chime")}
+          />
+          <label htmlFor="chime">Play chime when opening mic</label>
+        </div>
+      </div>
+      <div className="checkbox-wrapper">
+        <div className="styled-checkbox">
+          <input
+            id="mic-state"
+            type="checkbox"
+            checked={userSettings.listenForFollowup}
+            onChange={onPreferenceChange("listenForFollowup")}
+          />
+          <label htmlFor="mic-state">
+            Keep the microphone on for follow up responses
+          </label>
+        </div>
       </div>
     </fieldset>
   );

commit eea85842013a387ac4906323c3f3670480865cbb

Fix #1569, fix card display
Also fixes #1474. This is a regression from  #1296

extension/popup/popupView.jsx
@@ -801,9 +801,9 @@ const SearchResultsContent = ({
   return (
     <React.Fragment>
       <TextDisplay displayText={displayText} />
+      <div id="search-results">{renderCard()}</div>
       {renderFollowup ? null : (
         <React.Fragment>
-          <div id="search-results">{renderCard()}</div>
           <div id="search-footer">
             <IntentFeedback
               onSubmitFeedback={onSubmitFeedback}