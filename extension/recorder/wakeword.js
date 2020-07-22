0f9768418ad78bd5bb211ec422c352c663a77b62
Julia Cambre <jcambre@alumni.stanford.edu>
2020-07-22 12:31:58 -0400

    Minimum working version of the honkling wakeword

--- extension/recorder/wakeword.js
+++ extension/recorder/wakeword.js
@@ -5,6 +5,27 @@ import * as settings from "../settings.js";
 let enabled = false;
 
 function startWatchword(keywords, sensitivity) {
+  let micAudioProcessor = new MicAudioProcessor(audioConfig);
+  let model = new SpeechResModel("RES8", commands);
+  let inferenceEngine = new InferenceEngine(inferenceEngineConfig, commands);
+
+  micAudioProcessor.getMicPermission().done(function() {
+    setInterval(function() {
+      let offlineProcessor = new OfflineAudioProcessor(audioConfig, micAudioProcessor.getData());
+      offlineProcessor.getMFCC().done(function(mfccData) {
+
+        let command = inferenceEngine.infer(mfccData, model, commands);
+        // console.log("RECEIVED COMMAND ", command);
+
+        if (inferenceEngine.sequencePresent()) {
+          console.log("HEY FIREFOX");
+        }
+      });
+    }, predictionFrequency);
+  }).fail(function() {
+    alert('mic permission is required, please enable the mic usage!');
+  });
+
   log.info(
     "Would listening for watchwords:",
     keywords.join(", "),
