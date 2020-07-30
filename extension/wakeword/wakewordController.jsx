2c468fe976bcab314bea633742b5c21916eb9543
Julia Cambre <jcambre@alumni.stanford.edu>
2020-07-30 17:24:24 -0400

    Fixes #1854 - new design for the pinned wakeword tab (#1855)
    * Display randomized suggestions on the wakeword tab

--- extension/wakeword/wakewordController.jsx
+++ extension/wakeword/wakewordController.jsx
@@ -1,19 +1,28 @@
 /* globals ReactDOM */
 import * as settings from "../settings.js";
 import { updateWakeword } from "../recorder/wakeword.js";
+import { sendMessage } from "../communicate.js";
 // eslint-disable-next-line no-unused-vars
 import { WakewordView } from "./wakewordView.js";
 
 const wakewordContainer = document.getElementById("wakeword-container");
 
-export const WakewordController = function({ userSettings }) {
-  return <WakewordView userSettings={userSettings} />;
+export const WakewordController = function({ userSettings, suggestions }) {
+  return <WakewordView userSettings={userSettings} suggestions={suggestions} />;
 };
 
 async function render() {
   const userSettings = await settings.getSettings();
+  const suggestions = await sendMessage({
+    type: "getExamples",
+    number: 3,
+  });
+
   ReactDOM.render(
-    <WakewordController userSettings={userSettings} />,
+    <WakewordController
+      userSettings={userSettings}
+      suggestions={suggestions}
+    />,
     wakewordContainer
   );
   setTimeout(() => {
