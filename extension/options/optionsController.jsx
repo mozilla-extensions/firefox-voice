a72f16a5a2f6cc0bf4f12d1a4dc3c4e3122f4f34
Julia Cambre <jcambre@alumni.stanford.edu>
2020-08-05 13:35:18 -0400

    Fixes #1825 add locale as a preference (#1883)
    * Preliminary options selector for locales
    * Use navigator.language as the default language (if it's English)
    
    Co-authored-by: Ian Bicking <ian@ianbicking.org>

--- extension/options/optionsController.jsx
+++ extension/options/optionsController.jsx
@@ -83,6 +83,73 @@ export const OptionsController = function() {
   onKeyboardShortcutError = setKeyboardShortcutError;
   onTabChange = setTabValue;
 
+  const inputLocales = [
+    {
+      name: "English (Australia)",
+      code: "en-AU",
+    },
+    {
+      name: "English (Canada)",
+      code: "en-CA",
+    },
+    {
+      name: "English (Ghana)",
+      code: "en-GH",
+    },
+    {
+      name: "English (Hong Kong)",
+      code: "en-HK",
+    },
+    {
+      name: "English (India)",
+      code: "en-IN",
+    },
+    {
+      name: "English (Ireland)",
+      code: "en-IE",
+    },
+    {
+      name: "English (Kenya)",
+      code: "en-KE",
+    },
+    {
+      name: "English (New Zealand)",
+      code: "en-NZ",
+    },
+    {
+      name: "English (Nigeria)",
+      code: "en-NG",
+    },
+    {
+      name: "English (Pakistan)",
+      code: "en-PK",
+    },
+    {
+      name: "English (Philippines)",
+      code: "en-PH",
+    },
+    {
+      name: "English (Singapore)",
+      code: "en-SG",
+    },
+    {
+      name: "English (South Africa)",
+      code: "en-ZA",
+    },
+    {
+      name: "English (Tanzania)",
+      code: "en-TZ",
+    },
+    {
+      name: "English (United Kingdom)",
+      code: "en-GB",
+    },
+    {
+      name: "English (United States)",
+      code: "en-US",
+    },
+  ];
+
   useEffect(() => {
     if (!isInitialized) {
       isInitialized = true;
@@ -292,6 +359,7 @@ export const OptionsController = function() {
       useEditRoutineDraft={useEditRoutineDraft}
       audioInputDevices={audioInputDevices}
       synthesizedVoices={synthesizedVoices}
+      inputLocales={inputLocales}
     />
   );
 };
